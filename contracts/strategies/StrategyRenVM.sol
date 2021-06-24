003e1ec3f3fc1e5f6015d90fc487b503b7e46f6d
Alex Luce <alexanderluce@gmail.com>
2021-06-24 14:22:57 -0400

    improved tend function

--- contracts/strategies/StrategyRenVM.sol
+++ contracts/strategies/StrategyRenVM.sol
@@ -19,7 +19,9 @@ contract StrategyRenVM is StrategyAPI {
     
     address public immutable yearnStrategyPool;
     uint256 public reserveRenBTC;
-    uint256 public reserveWETH;
+    uint256 public reserveETH;
+    
+    address[] public ethPath;
     address public immutable governance;
     address public constant renBTC = 0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D;
     address public constant wETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
@@ -27,15 +29,23 @@ contract StrategyRenVM is StrategyAPI {
     address public constant ROUTER =
         0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
+    address[] public renBTCPath;
+    address[] public wETHPath;
+    address[] public wETHtoRenBTCPath;
+    
+    
     modifier onlyGovernance() {
         require(msg.sender == governance, '!governance');
         _;
     }
 
-    constructor(address _yearnStrategyPool, address _governance, uint256 _reserveRenBTC, uint256 _reserveWETH) {
+    constructor(address _yearnStrategyPool, address _governance, address[] memory _renBTCPath, address[] memory _wETHPath, address[] memory _wETHtoRenBTCPath, uint256 _reserveRenBTC, uint256 _reserveETH) {
         yearnStrategyPool = _yearnStrategyPool;
         reserveRenBTC = _reserveRenBTC;
-        reserveWETH = _reserveWETH;
+        renBTCPath = _renBTCPath;
+        wETHPath = _wETHPath;
+        wETHtoRenBTCPath = _wETHtoRenBTCPath;
+        reserveETH = _reserveETH;
         governance = _governance;
     }
 
@@ -82,35 +92,53 @@ contract StrategyRenVM is StrategyAPI {
         reserveRenBTC = want;
     }
 
+    function setMinimumETH(uint256 want) virtual external onlyGovernance {
+        reserveETH = want;
+    }
+
+    function setRenBTCPath(address[] memory _renBTCPath) external onlyGovernance {
+        renBTCPath = _renBTCPath;
+    }
+
+    function setWETHPath(address[] memory _wETHPath) external onlyGovernance {
+        wETHPath = _wETHPath;
+    }
+
 
     /*
     Estimate the total value of ren managed by the strategy
     */
     function estimatedTotalAssets() virtual override external view returns (uint256) {
         uint256 renBalance = IERC20(renBTC).balanceOf(address(this));
-        address[] memory renBTCPath = new address[](3);
-        renBTCPath[0] = renBTC;
-        renBTCPath[1] = wETH;
-        renBTCPath[2] = USDC;
         uint256 renBTCValue = IUniswapV2Router02(ROUTER).getAmountsOut(renBalance, renBTCPath)[renBTCPath.length-1];
-        //TODO add calculation for vault assets and eth assets?
-        return renBTCValue;
+
+        uint256 ethBalance = address(this).balance;
+        uint256 ethValue = IUniswapV2Router02(ROUTER).getAmountsOut(ethBalance, wETHPath)[wETHPath.length-1];
+
+        return renBTCValue + ethValue;
     }
 
     /*
     If trigger should be called, will signal it to the keeper. Should not ever return same as harvestTrigger.
     */
     function tendTrigger(uint256 callCost) virtual override external view returns (bool) {
-        return bool(IERC20(renBTC).balanceOf(address(this)) < reserveRenBTC);
+        bool criticalRenBTC = bool(IERC20(renBTC).balanceOf(address(this)) < reserveRenBTC);
+        bool criticalETH = bool(address(this).balance < reserveETH);
+        return !(criticalRenBTC || criticalETH);
     }
 
     function tend() virtual override external {
         uint256 renBTCBalance = IERC20(renBTC).balanceOf(address(this));
-        if (renBTCBalance < reserveRenBTC) {
-            uint256 shortageRenBTC = reserveRenBTC - renBTCBalance;
-            IyVault(yearnStrategyPool).withdraw(shortageRenBTC);
-        } else {
-            revert('Nothing to tend');
+        uint256 ethBalance = address(this).balance;
+
+        uint256 wantRenBTC = renBTCBalance < reserveRenBTC ? reserveRenBTC - renBTCBalance : 0;
+        uint256 wantETHinRenBTC = ethBalance < reserveETH ? IUniswapV2Router02(ROUTER).getAmountsIn(reserveETH - ethBalance, wETHtoRenBTCPath)[0] : 0;
+        IyVault(yearnStrategyPool).withdraw(wantRenBTC+wantETHinRenBTC);
+
+        if (wantETHinRenBTC!=0) {
+            require(IERC20(renBTC).approve(address(ROUTER), wantETHinRenBTC), 'approve failed.');
+            uint256 expectedEthOut = reserveETH - ethBalance;
+            IUniswapV2Router02(ROUTER).swapExactTokensForETH(wantETHinRenBTC, reserveETH - ethBalance, wETHtoRenBTCPath, address(this), block.timestamp+1);
         }
     }
 
