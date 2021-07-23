266605921792403b1802945f582f44cc9d1c8a26
Alex Luce <alex@framework.ventures>
2021-07-23 15:18:12 +0000

    refactor StrategyRenVM and switch router to Sushi

--- contracts/strategies/StrategyRenVM.sol
+++ contracts/strategies/StrategyRenVM.sol
@@ -18,24 +18,20 @@ contract StrategyRenVM {
 	using Address for address;
 	using SafeMath for uint256;
 
-	address public constant sBTCPool = 0x7fC77b5c7614E1533320Ea6DDc2Eb61fa00A9714;
-	int128 public constant renbtcIndex = 0;
-	int128 public constant wbtcIndex = 1;
+	address public constant curvePool = 0x7fC77b5c7614E1533320Ea6DDc2Eb61fa00A9714;
+	address public constant vault = address(0xA696a63cc78DfFa1a63E9E50587C197387FF6C7E);
 
 	address public constant want = address(0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D);
-	address public constant vault = address(0xA696a63cc78DfFa1a63E9E50587C197387FF6C7E);
+	int128 public constant wantIndex = 0;
+
+	address public constant vaultWant = address(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
+	int128 public constant vaultWantIndex = 1;
+
 	string public constant name = '0confirmation RenVM Strategy';
 	bool public constant isActive = true;
 
-	address public constant vaultWant = address(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
-
-	uint256 public reserveRenBTC;
-	uint256 public reserveETH;
-
 	uint256 public constant reserve = 200000000;
-
 	address public immutable controller;
-
 	address public governance;
 	address public strategist;
 
@@ -50,50 +46,45 @@ contract StrategyRenVM {
 		controller = _controller;
 	}
 
+	function swap(
+		address _tokenIn,
+		uint256 _amountIn,
+		int128 _indexIn,
+		int128 _indexOut
+	) internal returns (uint256) {
+		IERC20(_tokenIn).safeApprove(address(curvePool), _amountIn);
+		uint256 _expectedAmountOut = ICurvePool(curvePool).get_dy(_indexIn, _indexOut, _amountIn).sub(1); //Subtract 1 to avoid minimum token exchange errors
+		ICurvePool(curvePool).exchange(_indexIn, _indexOut, _amountIn, _expectedAmountOut);
+		return _expectedAmountOut;
+	}
+
 	function deposit() external virtual {
 		uint256 _want = IERC20(want).balanceOf(address(this)); //amount of tokens we want
-		console.log('Currently have', _want);
 		if (_want > reserve) {
-			uint256 _overflow = _want.sub(reserve);
-			//TODO should be min threshold, not hard coded
-
-			uint256 _amountOut = ICurvePool(sBTCPool).get_dy(renbtcIndex, wbtcIndex, _overflow).sub(1);
-			IERC20(want).safeApprove(address(sBTCPool), _overflow);
-			ICurvePool(sBTCPool).exchange(renbtcIndex, wbtcIndex, _overflow, _amountOut);
-			uint256 _actualOut = _amountOut;
-
-			IERC20(vaultWant).safeApprove(address(vault), _actualOut);
-			IyVault(vault).deposit(_actualOut);
-			uint256 estimatedShares = IyVault(vault).pricePerShare().mul(_actualOut).div(10**8);
+			uint256 _amountOut = swap(want, _want.sub(reserve), wantIndex, vaultWantIndex);
+			IERC20(vaultWant).safeApprove(address(vault), _amountOut);
+			IyVault(vault).deposit(_amountOut);
 		}
 	}
 
 	function _withdraw(uint256 _amount) private returns (uint256) {
-		// _amount is the number of renBTC we would like.
-		uint256 _vaultWant = ICurvePool(sBTCPool).get_dy(wbtcIndex, renbtcIndex, _amount);
-		// _vaultWant is the number of wBTC we would need.
-
-		uint256 _shares = estimateShares(_vaultWant);
+		uint256 _amount = ICurvePool(curvePool).get_dy(vaultWantIndex, wantIndex, _amount);
+		uint256 _shares = estimateShares(_amount);
 		IyVault(vault).withdraw(_shares);
-		IERC20(vaultWant).safeApprove(sBTCPool, _vaultWant);
-		uint256 _amountOut = ICurvePool(sBTCPool).get_dy(wbtcIndex, renbtcIndex, _vaultWant).sub(1);
-		ICurvePool(sBTCPool).exchange(wbtcIndex, renbtcIndex, _vaultWant, _amountOut);
+		uint256 _amountOut = swap(vaultWant, _amount, vaultWantIndex, wantIndex);
 		return _amountOut;
 	}
 
 	function withdraw(uint256 _amount) external virtual onlyController {
-		uint256 _amountOut = _withdraw(_amount);
-		IERC20(want).transfer(address(controller), _amountOut);
+		IERC20(want).transfer(address(controller), _withdraw(_amount));
 	}
 
 	function withdrawAll() external virtual onlyController {
-		uint256 _amount = IERC20(vault).balanceOf(address(this));
-		_withdraw(_amount);
+		_withdraw(IERC20(vault).balanceOf(address(this)));
 	}
 
 	function balanceOf() external view virtual returns (uint256) {
 		return IyVault(vault).balanceOf(address(this));
-		//return IyVault(vault).balanceOf(address(this)).add(IERC20(want).balanceOf(address(this))); TODO uncomment
 	}
 
 	function estimateShares(uint256 _amount) internal virtual returns (uint256) {
