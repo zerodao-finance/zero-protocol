97e306dce595f044214383150dee3a65be29338d
Alex Luce <alex@framework.ventures>
2021-07-28 18:06:19 +0000

    change want to wBTC

--- contracts/modules/Swap.sol
+++ contracts/modules/Swap.sol
@@ -15,10 +15,10 @@ contract Swap {
 	address public immutable controller;
 	address public immutable governance;
 	uint256 public blockTimeout;
-	address public constant fiat = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-	address public constant wNative = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-	address public constant want = 0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D;
-	address public constant router = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F;
+	address public constant fiat = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; //USDC
+	address public constant wNative = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; //wETH
+	address public constant want = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599; //wBTC
+	address public constant router = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F; //Sushi V2
 
 	constructor(address _controller) {
 		controller = _controller;
@@ -46,9 +46,8 @@ contract Swap {
 		uint256 _nonce,
 		bytes memory _data
 	) public {
-		require(_asset == want, '!renbtc');
 		uint256 amountSwapped = swapTokens(want, fiat, _actual);
-		outstanding[_nonce] = SwapLib.SwapRecord({qty: amountSwapped, when: uint64(block.timestamp), token: want});
+		outstanding[_nonce] = SwapLib.SwapRecord({qty: amountSwapped, when: uint64(block.timestamp), token: _asset});
 	}
 
 	function swapTokens(
@@ -61,10 +60,14 @@ contract Swap {
 		_path[1] = wNative;
 		_path[2] = _tokenOut;
 		IERC20(_tokenIn).approve(router, _amountIn);
-		return
-			IUniswapV2Router02(router).swapExactTokensForTokens(_amountIn, 1, _path, address(this), block.timestamp)[
-				_path.length - 1
-			];
+		uint256 _amountOut = IUniswapV2Router02(router).swapExactTokensForTokens(
+			_amountIn,
+			1,
+			_path,
+			address(this),
+			block.timestamp
+		)[_path.length - 1];
+		return _amountOut;
 	}
 
 	function repayLoan(
@@ -75,7 +78,10 @@ contract Swap {
 		bytes memory _data
 	) public {
 		require(outstanding[_nonce].qty != 0, '!outstanding');
-		IERC20(_asset).safeTransfer(_to, outstanding[_nonce].qty);
+		console.log('balance', IERC20(fiat).balanceOf(address(this)));
+		//IERC20(_asset).safeTransfer(_to, outstanding[_nonce].qty);
+		console.log('Attempting safe transfer of', _actualAmount);
+		IERC20(fiat).safeTransfer(_to, outstanding[_nonce].qty);
 		delete outstanding[_nonce];
 	}
 
