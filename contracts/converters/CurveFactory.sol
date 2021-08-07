fe3b7e7cca951ec519f8cfaa4255679e74db89de
Alex Luce <alexanderluce@gmail.com>
2021-08-07 02:22:33 -0400

    update

--- contracts/converters/CurveFactory.sol
+++ contracts/converters/CurveFactory.sol
@@ -4,17 +4,19 @@ pragma solidity >=0.7.0;
 import {ICurvePool} from '../interfaces/ICurvePool.sol';
 import {IERC20} from 'oz410/token/ERC20/IERC20.sol';
 import {SafeMath} from 'oz410/math/SafeMath.sol';
+import 'hardhat/console.sol';
 
 contract ZeroCurveFactory {
-	ZeroCurveWrapper[] wrappers;
+	event CreateWrapper(address _wrapper);
 
 	function createWrapper(
 		int128 _tokenInIndex,
 		int128 _tokenOutIndex,
 		address _pool
-	) public {
+	) public returns (address) {
 		ZeroCurveWrapper wrapper = new ZeroCurveWrapper(_tokenInIndex, _tokenOutIndex, _pool);
-		wrappers.push(wrapper);
+		emit CreateWrapper(address(wrapper));
+		return address(wrapper);
 	}
 }
 
@@ -45,8 +47,10 @@ contract ZeroCurveWrapper {
 
 	function convert(address _module) external returns (uint256) {
 		uint256 _balance = IERC20(tokenInAddress).balanceOf(address(this));
+		require(IERC20(tokenInAddress).approve(pool, _balance), 'approve failed.');
 		uint256 _minOut = estimate(_balance).sub(1); //Subtract one for minimum in case of rounding errors
-		uint256 _actualOut = ICurvePool(pool).exchange(tokenInIndex, tokenOutIndex, _balance, _minOut);
+		uint256 _actualOut = ICurvePool(pool).exchange(tokenInIndex, tokenOutIndex, _balance, 1);
+		console.log('actual is', _actualOut);
 		IERC20(tokenOutAddress).transfer(msg.sender, _actualOut);
 		return _actualOut;
 	}
