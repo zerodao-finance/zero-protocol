4b6e8a8f6cdc652178ae5301958c9071283e860d
Alex Luce <alex@framework.ventures>
2021-08-19 14:51:29 +0000

    initial dummy vault

--- contracts/test/DummyVault.sol
+++ contracts/test/DummyVault.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.7.0;
 
 import {yVault} from '../vendor/yearn/vaults/yVault.sol';
+import {IERC20} from 'oz410/token/ERC20/ERC20.sol';
 
 contract DummyVault is yVault {
 	constructor(
@@ -10,4 +11,25 @@ contract DummyVault is yVault {
 		string memory _name,
 		string memory _symbol
 	) yVault(_token, _controller, _name, _symbol) {}
+}
+
+contract DummyVault {
+	address immutable want;
+	constructor(address _want) {
+		want = _want;
+	}
+
+	function estimateShares(uint256 _amount) external view returns (uint256) {
+		return _amount;
+	}
+
+	function deposit(uint256 _amount) {
+		IERC20(want).transferFrom(msg.sender, address(this), _amount);
+		_mint(msg.sender, _amount);
+	}
+
+	function withdraw(uint256 _amount) {
+		IERC20(address(this)).transferFrom(msg.sender, address(this), _amount);
+		IERC20(want).transfer(msg.sender, _amount);
+	}
 }
