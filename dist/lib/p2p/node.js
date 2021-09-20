2206a94b9b988b5bb5238371f1dbeabec02190de
altinak <altinak@zero.gg>
2021-09-20 08:30:27 -0400

    libp2p working!

--- dist/lib/p2p/node.js
+++ dist/lib/p2p/node.js
@@ -11,58 +11,58 @@ const GossipSub = require('libp2p-gossipsub');
 const WStar = require('libp2p-webrtc-star');
 const isBrowser = require('is-browser');
 const returnOp = (v) => v;
+const ln = (v) => ((console.log(v)), v);
 const presets = {
     lendnet: '/dns4/lendnet.0confirmation.com/tcp/443/wss/p2p-webrtc-star/',
-    zeronet: '/dns4/zeronet.0confirmation.com/tcp/443/wss/p2p-webrtc-star/'
+    zeronet: '/dns4/zeronet.0confirmation.com/tcp/443/wss/p2p-webrtc-star/',
 };
 const fromPresetOrMultiAddr = (multiaddr) => presets[multiaddr] || multiaddr;
 const wrtc = require('wrtc');
+console.log(wrtc);
 module.exports = {
     createNode: async (options) => {
-        const peerInfo = options.peerInfo || await PeerInfo.create();
+        const peerInfo = options.peerInfo || (await PeerInfo.create());
         const multiaddr = fromPresetOrMultiAddr(options.multiaddr);
         peerInfo.multiaddrs.add(multiaddr);
         const dhtEnable = typeof options.dht === 'undefined' || options.dht === true;
         const socket = await libp2p.create({
             peerInfo,
+            addresses: {
+                listen: [
+                    multiaddr
+                ]
+            },
             modules: {
-                transport: [TCP, WS, WStar],
+                transport: [WS, WStar],
                 streamMuxer: [Mplex],
                 connEncryption: [SECIO],
                 pubsub: GossipSub,
                 peerDiscovery: [Bootstrap],
-                dht: dhtEnable ? KadDHT : undefined
+                dht: dhtEnable ? KadDHT : undefined,
             },
             config: {
                 peerDiscovery: {
-                    bootstrap: {
+                    autoDial: true,
+                    [Bootstrap.tag]: {
                         enabled: true,
-                        list: [options.multiaddr + '/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64']
+                        list: [options.multiaddr + '/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64'],
                     }
                 },
                 transport: {
                     [WStar.prototype[Symbol.toStringTag]]: {
-                        upgrader: {
-                            localPeer: peerInfo.id,
-                            upgradeInbound: returnOp,
-                            upgradeOutbound: returnOp
-                        },
-                        wrtc: isBrowser ? undefined : wrtc
+                        wrtc: ln(!isBrowser && wrtc)
                     }
                 },
                 dht: {
                     enabled: dhtEnable,
-                    kBucketSize: 20
+                    kBucketSize: 20,
                 },
                 pubsub: {
                     enabled: true,
-                    emitSelf: false
-                }
-            }
+                    emitSelf: false,
+                },
+            },
         });
-        await socket.start();
-        socket.pubsub.start();
-        socket.multiaddr = multiaddr;
         return socket;
     },
 };
