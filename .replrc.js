80696980b12583ba8c04edfedcb84535afc05c03
altinak <altinak@zero.gg>
2021-09-20 20:14:33 +0000

    updates

--- .replrc.js
+++ .replrc.js
@@ -1,4 +1,7 @@
 
 var sdk = require('./');
+const multiaddr = '/dns4/lourdehaufen.dynv6.net/tcp/443/wss/p2p-webrtc-star/';
 
 var makeKeeper = async () => sdk.createZeroKeeper(await sdk.createZeroConnection('/dns4/lourdehaufen.dynv6.net/tcp/443/wss/p2p-webrtc-star/'));
+
+var makeUser = async () => sdk.createZeroUser(await sdk.createZeroConnection(multiaddr));
