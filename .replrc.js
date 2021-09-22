ce8d9c3fd6bfe5fa31510c4fbe438aefd6780a03
altinak <altinak@zero.gg>
2021-09-22 14:52:57 +0000

    update replrc

--- .replrc.js
+++ .replrc.js
@@ -2,6 +2,17 @@
 var sdk = require('./');
 const multiaddr = '/dns4/lourdehaufen.dynv6.net/tcp/443/wss/p2p-webrtc-star/';
 
-var makeKeeper = async () => sdk.createZeroKeeper(await sdk.createZeroConnection('/dns4/lourdehaufen.dynv6.net/tcp/443/wss/p2p-webrtc-star/'));
-
 var makeUser = async () => sdk.createZeroUser(await sdk.createZeroConnection(multiaddr));
+var peerId = require('peer-id');
+
+var handler = async (o) => {
+  console.log('got dispatch');
+  console.log(require('util').inspect(o, { colors: true, depth: 15 }));
+};
+
+
+var makeKeeper = async () => {
+  const keeper = await sdk.createZeroKeeper(await sdk.createZeroConnection('/dns4/lourdehaufen.dynv6.net/tcp/443/wss/p2p-webrtc-star/'));
+  keeper.setTxDispatcher(handler);
+  return keeper;
+};
