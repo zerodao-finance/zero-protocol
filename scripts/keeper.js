22bcef09a34a64d8a9f8e9935936bfea965642be
Alex Luce <alexanderluce@gmail.com>
2021-12-04 13:52:08 -0500

    u

--- scripts/keeper.js
+++ scripts/keeper.js
@@ -6,7 +6,7 @@ const { createZeroConnection, createZeroKeeper } = require('../lib/zero');
 
 /*--------------------------- ENVIRONMENT VARIABLES -------------------------*/
 
-// PK: The private key of the wallet to use.
+// WALLET: The private key of the wallet to use.
 
 //--------------------------------- CONSTANTS -------------------------------*/
 
@@ -21,11 +21,13 @@ const MAX_AMOUNT = 50000000;
 // URL of P2P network to use. DON'T MODIFY unless you know what you're doing...
 const KEEPER_URL = '/dns4/lourdehaufen.dynv6.net/tcp/443/wss/p2p-webrtc-star/'
 
+const CONTROLLER = '0x53f38bEA30fE6919e0475Fe57C2629f3D3754d1E'
+
 //-----------------------------------------------------------------------------
 
 const executeLoan = async (transferRequest) => {
     const [signer] = await ethers.getSigners();
-    const wallet = new Wallet(process.env.PK, signer.provider);
+    const wallet = new Wallet(process.env.WALLET, signer.provider);
 
     const loan = await transferRequest.loan(wallet);
     await loan.wait();
@@ -38,7 +40,7 @@ const executeLoan = async (transferRequest) => {
 
 const hasEnough = async (transferRequest) => {
     const [signer] = await ethers.getSigners();
-    const wallet = new Wallet(process.env.PK, signer);
+    const wallet = new Wallet(process.env.WALLET, signer);
 
     const balance = await (new Contract(await underwriter.controller(), ['function balanceOf(address _owner) returns (uint256 balance)'], signer)).balanceOf(wallet.address);
     return balance > transferRequest.amount
@@ -46,14 +48,16 @@ const hasEnough = async (transferRequest) => {
 
 const handleTransferRequest = async (message) => {
     try {
-        const contract = await loadContracts();
-        const transferRequest = new TrivialUnderwriterTransferRequest(message);
+        const transferRequest = new TrivialUnderwriterTransferRequest({ ...message, contractAddress: CONTROLLER });
 
-        if (!(hasEnough(transferRequest))) return;
-
+        //if (!(hasEnough(transferRequest))) return;
+        console.log("Submitting to renVM...")
         const mint = await transferRequest.submitToRenVM();
+        console.log("Successfully submitted to renVM.")
+        console.log("Gateway address is", await transferRequest.toGatewayAddress())
 
         await new Promise((resolve, reject) => mint.on('deposit', async (deposit) => {
+            console.log("Deposit received.")
             await resolve();
             const hash = deposit.txHash();
             const depositLog = (msg) => console.log(`RenVM Hash: ${hash}\nStatus: ${deposit.status}\n${msg}`);
