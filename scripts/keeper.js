bedcf04e36f27c3f29bfeb6c9cbbb2758e635106
altinak <altinak@zero.gg>
2021-12-04 18:20:09 -0500

    update

--- scripts/keeper.js
+++ scripts/keeper.js
@@ -50,7 +50,7 @@ const hasEnough = async (transferRequest) => {
 
 const handleTransferRequest = async (message) => {
     try {
-        const transferRequest = new TrivialUnderwriterTransferRequest({ ...message, chainId: 42161, contractAddress: CONTROLLER });
+        const transferRequest = new TrivialUnderwriterTransferRequest({ ...message, contractAddress: CONTROLLER });
         transferRequest.setUnderwriter(trivial.address);
 
         //if (!(hasEnough(transferRequest))) return;
