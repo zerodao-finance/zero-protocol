7eb8d6dcbf942f31bbf58f6b15aae6b3aa942041
altinak <altinak@zero.gg>
2022-01-25 17:24:00 -0500

    fix PolygonConvert.sol

--- contracts/modules/PolygonConvert.sol
+++ contracts/modules/PolygonConvert.sol
@@ -1,5 +1,6 @@
 pragma solidity >=0.6.0;
-import { PolygonConvertLib } from './PolygonConvertLib.sol';
+import {PolygonConvertLib} from './PolygonConvertLib.sol';
+import {IUniswapV2Router02} from '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
 import {SafeMath} from 'oz410/math/SafeMath.sol';
 import {IERC20} from 'oz410/token/ERC20/IERC20.sol';
 import {SafeERC20} from 'oz410/token/ERC20/SafeERC20.sol';
@@ -7,108 +8,101 @@ import {IController} from '../interfaces/IController.sol';
 import {IConverter} from '../interfaces/IConverter.sol';
 import {ICurveUInt256} from '../interfaces/CurvePools/ICurveUInt256.sol';
 import {IController} from '../interfaces/IController.sol';
-import {ICurveUnderlyingUInt256} from "../interfaces/CurvePools/ICurveUnderlyingUInt256.sol";
-import {ICurveInt128} from "../interfaces/CurvePools/ICurveInt128.sol";
+import {ICurveUnderlyingUInt256} from '../interfaces/CurvePools/ICurveUnderlyingUInt256.sol';
+import {ICurveInt128} from '../interfaces/CurvePools/ICurveInt128.sol';
 import {IRenCrvPolygon} from '../interfaces/CurvePools/IRenCrvPolygon.sol';
 
-
 contract PolygonConvert {
-    using SafeERC20 for *;
-    using SafeMath for *;
-    mapping(uint256 => PolygonConvertLib.ConvertRecord) public outstanding;
-    address public immutable controller;
-    address public immutable governance;
-    uint256 public blockTimeout;
-    address public constant wMatic = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
-    address public constant weth = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;
-    address public constant wbtc = 0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6;
-    address public constant want = 0xDBf31dF14B66535aF65AaC99C32e9eA844e14501;
-    address public constant renCrvPolygon = 0xC2d95EEF97Ec6C17551d45e77B590dc1F9117C67;
-    address public constant tricryptoPolygon = 0x92215849c439E1f8612b6646060B4E3E5ef822cC;
+	using SafeERC20 for *;
+	using SafeMath for *;
+	mapping(uint256 => PolygonConvertLib.ConvertRecord) public outstanding;
+	address public immutable controller;
+	address public immutable governance;
+	uint256 public blockTimeout;
+  address public constant router = 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506;
+	address public constant wMatic = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
+	address public constant weth = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;
+	address public constant wbtc = 0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6;
+	address public constant want = 0xDBf31dF14B66535aF65AaC99C32e9eA844e14501;
+	address public constant renCrvPolygon = 0xC2d95EEF97Ec6C17551d45e77B590dc1F9117C67;
+	address public constant tricryptoPolygon = 0x92215849c439E1f8612b6646060B4E3E5ef822cC;
 
-    modifier onlyController() {
-        require(msg.sender == controller, '!controller');
-        _;
+	modifier onlyController() {
+		require(msg.sender == controller, '!controller');
+		_;
+	}
+
+	constructor(address _controller) {
+		controller = _controller;
+		governance = IController(_controller).governance();
+    IERC20(want).safeApprove(router, ~uint256(0) >> 2);
+	}
+
+	function setBlockTimeout(uint256 _ct) public {
+		require(msg.sender == governance, '!governance');
+		blockTimeout = _ct;
+	}
+
+	function isActive(PolygonConvertLib.ConvertRecord storage record) internal view returns (bool) {
+		return record.qty != 0 || record.qtyETH != 0;
+	}
+
+	function defaultLoan(uint256 _nonce) public {
+		require(block.number >= outstanding[_nonce].when + blockTimeout);
+		require(isActive(outstanding[_nonce]), '!outstanding');
+		uint256 _amountSwappedBack = swapTokensBack(outstanding[_nonce]);
+		IERC20(want).safeTransfer(controller, _amountSwappedBack);
+		delete outstanding[_nonce];
+	}
+
+	function receiveLoan(
+		address _to,
+		address _asset,
+		uint256 _actual,
+		uint256 _nonce,
+		bytes memory _data
+	) public onlyController {
+		uint256 ratio = abi.decode(_data, (uint256));
+		(uint256 amountSwappedETH, uint256 amountSwappedBTC) = swapTokens(_actual, ratio);
+		outstanding[_nonce] = PolygonConvertLib.ConvertRecord({
+			qty: amountSwappedBTC,
+			when: uint64(block.timestamp),
+			qtyETH: amountSwappedETH
+		});
+	}
+
+	function swapTokens(uint256 _amountIn, uint256 _ratio)
+		internal
+		returns (uint256 amountSwappedETH, uint256 amountSwappedBTC)
+	{
+		uint256 amountToETH = _ratio.mul(_amountIn).div(uint256(1 ether));
+		if (amountToETH != 0) {
+      address[] memory path = new address[](2);
+      path[0] = want;
+      path[1] = wMatic;
+      uint256[] memory toMaticResult = IUniswapV2Router02(router).swapExactTokensForETH(amountToETH, 1, path, address(this), block.timestamp + 1);
+      amountSwappedETH = toMaticResult[1];
+      amountSwappedBTC = _amountIn.sub(amountToETH);
+    } else {
+      amountSwappedBTC = _amountIn;
     }
+	}
 
-    constructor(address _controller) {
-        controller = _controller;
-        governance = IController(_controller).governance();
-        IERC20(want).safeApprove(renCrvPolygon, ~uint256(0) >> 2);
-        IERC20(wbtc).safeApprove(tricryptoPolygon, ~uint256(0) >> 2);
-    }
-
-    function setBlockTimeout(uint256 _ct) public {
-        require(msg.sender == governance, '!governance');
-        blockTimeout = _ct;
-    }
-
-    function isActive(PolygonConvertLib.ConvertRecord storage record) internal view returns (bool) {
-        return record.qty != 0 || record.qtyETH != 0;
-    }
-
-    function defaultLoan(uint256 _nonce) public {
-        require(block.number >= outstanding[_nonce].when + blockTimeout);
-        require(isActive(outstanding[_nonce]), '!outstanding');
-        uint256 _amountSwappedBack = swapTokensBack(outstanding[_nonce]);
-        IERC20(want).safeTransfer(controller, _amountSwappedBack);
-        delete outstanding[_nonce];
-    }
-
-    function receiveLoan(
-        address _to,
-        address _asset,
-        uint256 _actual,
-        uint256 _nonce,
-        bytes memory _data
-    ) public onlyController {
-        uint256 ratio = abi.decode(_data, (uint256));
-        (uint256 amountSwappedETH, uint256 amountSwappedBTC) = swapTokens(_actual, ratio);
-        outstanding[_nonce] = PolygonConvertLib.ConvertRecord({
-            qty: amountSwappedBTC,
-            when: uint64(block.timestamp), 
-            qtyETH: amountSwappedETH
-        });
-    }
-
-    function swapTokens(uint256 _amountIn, uint256 _ratio)
-        internal
-        returns (uint256 amountSwappedETH, uint256 amountSwappedBTC)
-    {
-        uint256 amountToETH = _ratio.mul(_amountIn).div(uint256(1 ether));
-        uint256 wbtcOut = amountToETH != 0 
-            ? ICurveInt128(renCrvPolygon).exchange(1, 0, amountToETH, 0)
-			: 0;
-		if (wbtcOut != 0) {
-			uint256 _amountStart = address(this).balance;
-			IConverter converter = IConverter(IController(msg.sender).converters(wbtc, wMatic));
-			IERC20(wbtc).safeTransfer(address(converter), wbtcOut);
-			uint256 wMaticOut = converter.convert(address(this));
-			converter = IConverter(IController(msg.sender).converters(wMatic, address(0x0)));
-			IERC20(wMaticOut).safeTransfer(address(converter), wMaticOut);
-			converter.convert(address(this));
-			amountSwappedETH = address(this).balance.sub(_amountStart);
-			amountSwappedBTC = _amountIn.sub(amountToETH);
-		} else {
-			amountSwappedBTC = _amountIn;
-		}
-    }
-
-    receive() external payable {
+	receive() external payable {
 		//
-    }
+	}
 
-    function swapTokensBack(PolygonConvertLib.ConvertRecord storage record) internal returns (uint256 amountReturned) {
-        uint256 _amountStart = IERC20(wbtc).balanceOf(address(this));
-        (bool success, ) = tricryptoPolygon.call{value: record.qtyETH}(
-            abi.encodeWithSelector(ICurveUInt256.exchange.selector, 2, 1, record.qtyETH, 0)
-        );
-        require(success, '!exchange');
-        uint256 wbtcOut = IERC20(wbtc).balanceOf(address(this));
-        amountReturned = IRenCrvPolygon(renCrvPolygon).exchange(0, 1, wbtcOut, 0).add(record.qty);
-    }
+	function swapTokensBack(PolygonConvertLib.ConvertRecord storage record) internal returns (uint256 amountReturned) {
+		uint256 _amountStart = IERC20(wbtc).balanceOf(address(this));
+		(bool success, ) = tricryptoPolygon.call{value: record.qtyETH}(
+			abi.encodeWithSelector(ICurveUInt256.exchange.selector, 2, 1, record.qtyETH, 0)
+		);
+		require(success, '!exchange');
+		uint256 wbtcOut = IERC20(wbtc).balanceOf(address(this));
+		amountReturned = IRenCrvPolygon(renCrvPolygon).exchange(0, 1, wbtcOut, 0).add(record.qty);
+	}
 
-    function repayLoan(
+	function repayLoan(
 		address _to,
 		address _asset,
 		uint256 _actualAmount,
@@ -122,9 +116,7 @@ contract PolygonConvert {
 		delete outstanding[_nonce];
 	}
 
-    function computeReserveRequirement(uint256 _in) external view returns (uint256) {
+	function computeReserveRequirement(uint256 _in) external view returns (uint256) {
 		return _in.mul(uint256(1e17)).div(uint256(1 ether));
 	}
-
-
 }
