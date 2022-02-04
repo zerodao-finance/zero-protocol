649adb4d7784c4159454c51f1927ac1de5be875d
yoyobojo <angellolazar@gmail.com>
2022-02-04 00:06:21 -0500

    deployed zero contracts

--- deploy/2_deploy_mainnet.js
+++ deploy/2_deploy_mainnet.js
@@ -1,6 +1,6 @@
 const hre = require("hardhat")
-const { ethers, deployments } = hre;
-import { Contract } from 'ethers';
+const { ethers, deployments, upgrades } = hre;
+import { Contract, utils } from 'ethers';
 
 const deployFixedAddress = async (...args) => {
     console.log('Deploying ' + args[0]);
@@ -11,6 +11,15 @@ const deployFixedAddress = async (...args) => {
     const result = await deployments.deploy(...args);
     //  restoreSigner(signer);
     console.log('Deployed to ' + result.address);
+    return result;
+};
+
+const deployProxyFixedAddress = async (...args) => {
+    console.log('Deploying proxy');
+    //const [signer] = await ethers.getSigners();
+    //hijackSigner(signer);
+    const result = await upgrades.deployProxy(...args);
+    //restoreSigner(signer);
     return result;
 };
 
@@ -41,7 +50,28 @@ module.exports = async ({
     // For testing airdrop - Start
     const { abi: erc20abi } = await deployments.getArtifact('BTCVault');
     const [testTreasury] = await ethers.getSigners();
+    // Zero / BTC
     const renBTC = new Contract(deployParameters['ETHEREUM']['renBTC'], erc20abi, testTreasury);
+    console.log("RenBTC Address: ", renBTC.address);
+    console.log("RenBTC Interface: ", renBTC.interface.format(utils.FormatTypes.minimal));
+    const zeroUnderwriterLockBytecodeLib = await deployFixedAddress('ZeroUnderwriterLockBytecodeLib', {
+        contractName: 'ZeroUnderwriterLockBytecodeLib',
+        args: [],
+        from: deployer
+    });
+    const zeroControllerFactory = (await hre.ethers.getContractFactory("ZeroController", {
+        libraries: {
+            ZeroUnderwriterLockBytecodeLib: zeroUnderwriterLockBytecodeLib.address
+        }
+    }));
+    const zeroController = await deployProxyFixedAddress(zeroControllerFactory, ["0x0F4ee9631f4be0a63756515141281A3E2B293Bbe", deployParameters["ETHEREUM"].gatewayRegistry], {
+        unsafeAllowLinkedLibraries: true
+    });
+    const BTCVault = await deployFixedAddress('BTCVault', {
+        contractName: 'BTCVault',
+        args: [deployParameters['ETHEREUM']['renBTC'], zeroController.address, "zeroBTC", "zBTC"],
+        from: deployer
+    });
     // For testing airdrop - End
 
     const zeroToken = await deployFixedAddress("ZERO", {
