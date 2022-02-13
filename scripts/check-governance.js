7c90dc306d69f78b43fe783d69466eb5b400ff90
Enzo Yono <nicholas.khami@gmail.com>
2022-02-12 20:54:06 -0600

    changed check-governance to work

--- scripts/check-governance.js
+++ scripts/check-governance.js
@@ -1,19 +1,22 @@
+import { ethers } from "ethers";
+import hre from 'hardhat'
+
 'use strict';
 
-const { createGetGasPrice } = require('ethers-polygongastracker');
+// const { createGetGasPrice } = require('ethers-polygongastracker');
 
-const getGasPrice = createGetGasPrice();
+// const getGasPrice = createGetGasPrice();
+const { deployments } = hre;
+const provider = new ethers.providers.JsonRpcProvider('https://arbitrum-mainnet.infura.io/v3/816df2901a454b18b7df259e61f92cd2')
 
-const hre = require('hardhat');
-const { BaseProvider } = hre.ethers.providers;
-
-BaseProvider.prototype.getGasPrice = async () => {
-  const p = await getGasPrice();
-  return p.mul(15);
-};
+// BaseProvider.prototype.getGasPrice = async () => {
+//   const p = await getGasPrice();
+//   return p.mul(15);
+// };
 
 const main = async () => {
-  const controller = await hre.ethers.getContract('ZeroController');
+  const zeroControllerABI = (await deployments.getArtifact('ZeroController')).abi;
+  const controller = new ethers.Contract('0x53f38bEA30fE6919e0475Fe57C2629f3D3754d1E', zeroControllerABI, provider);
   console.log(await controller.governance());
 };
 
