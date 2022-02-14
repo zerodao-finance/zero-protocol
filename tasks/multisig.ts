6b45ac18491988f0c98ccc0d466074313fdde748
capybara <capybara@capybara>
2022-02-14 20:41:11 +0530

    chore: now calls the contract directly

--- tasks/multisig.ts
+++ tasks/multisig.ts
@@ -1,67 +1,89 @@
 // @ts-ignore
-import { task } from 'hardhat/config'
-import Safe from '@gnosis.pm/safe-core-sdk'
-import SafeServiceClient from '@gnosis.pm/safe-service-client'
-import EthersAdapter from '@gnosis.pm/safe-ethers-lib'
+import { task } from 'hardhat/config';
+import Safe from '@gnosis.pm/safe-core-sdk';
+import SafeServiceClient from '@gnosis.pm/safe-service-client';
+import EthersAdapter from '@gnosis.pm/safe-ethers-lib';
 
 const getSigner = async (ethers) => {
-    const [signer] = await ethers.getSigners.call(ethers);
-    return new ethers.Wallet(
-        process.env.WALLET,
-        process.env.FORKING === 'true'
-            ? signer.provider
-            : new ethers.providers.JsonRpcProvider(
-                'https://arbitrum-mainnet.infura.io/v3/816df2901a454b18b7df259e61f92cd2',
-            ),
-    );
+	const [signer] = await ethers.getSigners.call(ethers);
+	return new ethers.Wallet(
+		process.env.WALLET,
+		process.env.FORKING === 'true'
+			? signer.provider
+			: new ethers.providers.JsonRpcProvider(
+					'https://arbitrum-mainnet.infura.io/v3/816df2901a454b18b7df259e61f92cd2',
+			  ),
+	);
 };
 
 const getContract = async (to, ethers) => {
-    return await (await ethers.getContract(to).then(d => d.address).catch((e) => ethers.utils.getAddress(to)))
-}
+	return await await ethers
+		.getContract(to)
+		.then((d) => d.address)
+		.catch((e) => ethers.utils.getAddress(to));
+};
 
 //@ts-ignore
-task("multisig", "sends out a multisig proposal")
-    .addOptionalParam("data", "encoded data")
-    .addOptionalParam("to", "to contract")
-    .addParam("safe", "safe address")
-    .addOptionalParam("execute", "execute transaction")
-    //@ts-ignore
-    .setAction(async ({ to, data, safe, execute }, { ethers, network }) => {
-       const serviceUrl = (() => {
-           const networkName = (() => {switch(network.name) {
-                case 'matic':
-                    return
-                case 'arbitrum':
-           }})()
-           return `https://safe-transaction.${networkName}.gnosis.io`
-        })()
-        const safeService = new SafeServiceClient(serviceUrl)
-        const contract = await getContract(to, ethers)
-        const signer = await getSigner(ethers)
-        const owner = new EthersAdapter({
-            ethers,
-            signer
-        })
-        const safeSdk = await Safe.create({ safeAddress: safe, ethAdapter: owner })
-        const safeTx = await safeSdk.createTransaction({ data, to: contract, value: "0" })
-        const hash = await safeSdk.getTransactionHash(safeTx)
-        await safeSdk.signTransaction(safeTx)
-        await safeService.proposeTransaction({
-            safeAddress: safe,
-            safeTransaction: safeTx,
-            safeTxHash: hash,
-            senderAddress: await signer.getAddress()
-        }).catch(async e => {
-            await safeService.confirmTransaction(hash, safeTx.signatures.get(await signer.getAddress())?.data)
-        })
-        // const tx = await safeSdk.approveTransactionHash(hash)
+task('multisig', 'sends out a multisig proposal')
+	.addOptionalParam('data', 'encoded data')
+	.addOptionalParam('to', 'to contract')
+	.addParam('safe', 'safe address')
+	.addOptionalParam('execute', 'execute transaction')
+	//@ts-ignore
+	.setAction(async ({ to, data, safe, execute }, { ethers, network }) => {
+		const serviceUrl = (() => {
+			const networkName = (() => {
+				switch (network.name) {
+					case 'matic':
+						return;
+					case 'arbitrum':
+				}
+			})();
+			return `https://safe-transaction.${networkName}.gnosis.io`;
+		})();
+		const safeService = new SafeServiceClient(serviceUrl);
+		const contract = await getContract(to, ethers);
+		const signer = await getSigner(ethers);
+		const owner = new EthersAdapter({
+			ethers,
+			signer,
+		});
+		const safeSdk = await Safe.create({ safeAddress: safe, ethAdapter: owner });
+		const safeTx = await safeSdk.createTransaction({ data, to: contract, value: '0' });
+		const hash = await safeSdk.getTransactionHash(safeTx);
+		await safeSdk.signTransaction(safeTx);
+		await safeService
+			.proposeTransaction({
+				safeAddress: safe,
+				safeTransaction: safeTx,
+				safeTxHash: hash,
+				senderAddress: await signer.getAddress(),
+			})
+			.catch(async (e) => {
+				await safeService.confirmTransaction(hash, safeTx.signatures.get(await signer.getAddress())?.data);
+			});
+		// const tx = await safeSdk.approveTransactionHash(hash)
 
-        if (execute) {
-            const safeContract = new ethers.Contract(safe, ["getTransactionHash(address,uint256,bytes,uint256,uint256,uint256,uint256,address,address,uint256) returns (bytes32)"])
-            const signedSafeTx = await safeService.getTransaction(hash)
-            console.log(signedSafeTx)
-          // TODO: this
-        }
-
-    })
+		if (execute) {
+			const safeContract = new ethers.Contract(safe, [
+				'getTransactionHash(address,uint256,bytes,uint256,uint256,uint256,uint256,address,address,uint256) returns (bytes32)',
+				'execTransaction(address,uint256,bytes,uint256,uint256,uint256,uint256,address,address,bytes) payable returns (bool)',
+			]);
+			const signedSafeTx = await safeService.getTransaction(hash);
+			console.log(signedSafeTx.transactionHash);
+			// TODO: this
+          const tx = await safeContract.execTransaction(
+              signedSafeTx.to,
+              signedSafeTx.value,
+              signedSafeTx.data,
+              signedSafeTx.operation,
+              signedSafeTx.safeTxGas,
+              signedSafeTx.baseGas,
+              signedSafeTx.gasPrice,
+              signedSafeTx.gasToken,
+              signedSafeTx.refundReceiver,
+              signedSafeTx.signatures
+          )
+            await tx.wait()
+		}
+	});
