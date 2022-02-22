7faacf80634b5f5706f1f74f1c1c0f14496342ed
UndercovercameL <undercover@camel>
2022-02-22 10:24:42 +0530

    chore: init MetaRequest

--- lib/MetaRequest.ts
+++ lib/MetaRequest.ts
@@ -1,1 +1,182 @@
-export class MetaRequest {}
+import { Wallet } from '@ethersproject/wallet';
+import { Signer } from '@ethersproject/abstract-signer';
+import { hexlify } from '@ethersproject/bytes';
+import { randomBytes } from '@ethersproject/random';
+import { _TypedDataEncoder } from '@ethersproject/hash';
+import { GatewayAddressInput } from './types';
+import { recoverAddress } from '@ethersproject/transactions';
+import { Buffer } from 'buffer';
+import { BigNumberish, ethers } from 'ethers';
+import { signTypedDataUtils } from '@0x/utils';
+import { EIP712TypedData } from '@0x/types';
+import { EIP712_TYPES } from './config/constants';
+import { Bitcoin } from '@renproject/chains';
+import RenJS from '@renproject/ren';
+import { EthArgs } from '@renproject/interfaces';
+import { getProvider } from './deployment-utils';
+
+export class MetaRequest {
+	public module: string;
+	public addressFrom: string;
+	public underwriter: string;
+	public asset: string;
+	public nonce: string;
+	public pNonce: string;
+	public data: string;
+	public contractAddress: string;
+	public chainId: number | string;
+	public signature: string;
+	private _destination: string;
+	private _contractFn: string;
+	private _contractParams: EthArgs;
+	private _ren: RenJS;
+	public _queryTxResult: any;
+	public provider: any;
+	public _mint: any;
+
+	constructor(params: {
+		module: string;
+		addressFrom: string;
+		underwriter: string;
+		asset: string;
+		data: string;
+		nonce?: BigNumberish;
+		pNonce?: BigNumberish;
+		contractAddress?: string;
+		chainId?: number;
+		signature?: string;
+	}) {
+		this.module = params.module;
+		this.addressFrom = params.addressFrom;
+		this.underwriter = params.underwriter;
+		this.asset = params.asset;
+		this.data = params.data;
+		console.log('params.nonce', params.nonce);
+		this.nonce = params.nonce ? hexlify(params.nonce) : hexlify(randomBytes(32));
+		this.pNonce = params.pNonce ? hexlify(params.pNonce) : hexlify(randomBytes(32));
+		this.chainId = params.chainId;
+		this.contractAddress = params.contractAddress;
+		this.signature = params.signature;
+		//this._config =
+		this._ren = new (RenJS as any)('mainnet', { loadCompletedDeposits: true });
+		this._contractFn = 'zeroCall';
+		this._contractParams = [
+			{
+				name: 'from',
+				type: 'address',
+				value: this.addressFrom,
+			},
+			{
+				name: 'pNonce',
+				type: 'uint256',
+				value: this.pNonce,
+			},
+			{
+				name: 'module',
+				type: 'address',
+				value: this.module,
+			},
+			{
+				name: 'data',
+				type: 'bytes',
+				value: this.data,
+			},
+		];
+	}
+
+	destination(contractAddress?: string, chainId?: number | string, signature?: string) {
+		if (this._destination) return this._destination;
+		const payload = this.toEIP712(contractAddress || this.contractAddress, Number(chainId || this.chainId));
+		delete payload.types.EIP712Domain;
+		const digest = _TypedDataEncoder.hash(payload.domain, payload.types, payload.message);
+		return (this._destination = recoverAddress(digest, signature || this.signature));
+	}
+	setProvider(provider) {
+		this.provider = provider;
+		return this;
+	}
+	async submitToRenVM(isTest) {
+		console.log('submitToRenVM this.nonce', this.nonce);
+		if (this._mint) return this._mint;
+		const result = (this._mint = await this._ren.lockAndMint({
+			asset: 'BTC',
+			from: Bitcoin(),
+			nonce: this.nonce,
+			to: getProvider(this).Contract({
+				sendTo: this.contractAddress,
+				contractFn: this._contractFn,
+				contractParams: this._contractParams,
+			}),
+		}));
+		//    result.params.nonce = this.nonce;
+		return result;
+	}
+	async waitForSignature() {
+		if (this._queryTxResult) return this._queryTxResult;
+		const mint = await this.submitToRenVM(false);
+		const deposit: any = await new Promise((resolve, reject) => {
+			mint.on('deposit', resolve);
+			(mint as any).on('error', reject);
+		});
+		await deposit.signed();
+		const { signature, nhash, phash, amount } = deposit._state.queryTxResult.out;
+		const result = (this._queryTxResult = {
+			amount: String(amount),
+			nHash: hexlify(nhash),
+			pHash: hexlify(phash),
+			signature: hexlify(signature),
+		});
+		return result;
+	}
+	setUnderwriter(underwriter: string): boolean {
+		if (!ethers.utils.isAddress(underwriter)) return false;
+		this.underwriter = ethers.utils.getAddress(underwriter);
+		return true;
+	}
+
+	toEIP712Digest(contractAddress: string, chainId?: number): Buffer {
+		return signTypedDataUtils.generateTypedDataHash(
+			this.toEIP712(contractAddress || this.contractAddress, Number(chainId || this.chainId)),
+		);
+	}
+
+	toEIP712(contractAddress: string, chainId?: number): EIP712TypedData {
+		this.contractAddress = contractAddress || this.contractAddress;
+		this.chainId = chainId || this.chainId;
+		return {
+			types: EIP712_TYPES,
+			domain: {
+				name: 'ZeroController',
+				version: '1',
+				chainId: String(this.chainId) || '1',
+				verifyingContract: this.contractAddress || ethers.constants.AddressZero,
+			},
+			message: {
+				module: this.module,
+				asset: this.asset,
+				data: this.data,
+				underwriter: this.underwriter,
+				nonce: this.pNonce,
+			},
+			primaryType: 'MetaRequest',
+		};
+	}
+	async toGatewayAddress(input: GatewayAddressInput): Promise<string> {
+		const mint = await this.submitToRenVM(false);
+		return mint.gatewayAddress;
+	}
+	async sign(signer: Wallet & Signer, contractAddress?: string): Promise<string> {
+		const provider = signer.provider as ethers.providers.JsonRpcProvider;
+		const { chainId } = await signer.provider.getNetwork();
+		try {
+			const payload = this.toEIP712(contractAddress, chainId);
+			delete payload.types.EIP712Domain;
+			return (this.signature = await signer._signTypedData(payload.domain, payload.types, payload.message));
+		} catch (e) {
+			return (this.signature = await provider.send('eth_signTypedData_v4', [
+				await signer.getAddress(),
+				this.toEIP712(this.contractAddress || contractAddress, chainId),
+			]));
+		}
+	}
+}
