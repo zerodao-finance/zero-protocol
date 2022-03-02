f7ed4fa05dd0525c466a4dee78246cda27cfb3bd
Alex Luce <alex@framework.ventures>
2022-03-02 20:21:28 +0000

    impl

--- contracts/vaults/BTCVault.sol
+++ contracts/vaults/BTCVault.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0<0.8.0;
+pragma solidity >=0.7.0 <0.8.0;
 
-import {yVault} from '../vendor/yearn/vaults/yVault.sol';
+import {yVaultUpgradeable} from '../vendor/yearn/vaults/yVaultUpgradeable.sol';
 
-contract BTCVault is yVault {
-	constructor(
+contract BTCVault is yVaultUpgradeable {
+	function initialize(
 		address _token,
 		address _controller,
 		string memory _name,
 		string memory _symbol
-	) yVault(_token, _controller, _name, _symbol) {}
+	) public initializer {
+		__yVault_init_unchained(_token, _controller, _name, _symbol);
+	}
 }
