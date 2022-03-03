4d2a31698b6b43d6ee94c5172de17f026198c9c8
Alex Luce <alex@framework.ventures>
2022-03-03 23:03:05 +0000

    replace boringcrypto with openzeppelin

--- contracts/interfaces/IRewarder.sol
+++ contracts/interfaces/IRewarder.sol
@@ -1,9 +1,21 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.6.12;
-import "@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol";
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
 interface IRewarder {
-    using BoringERC20 for IERC20;
-    function onSushiReward(uint256 pid, address user, address recipient, uint256 sushiAmount, uint256 newLpAmount) external;
-    function pendingTokens(uint256 pid, address user, uint256 sushiAmount) external view returns (IERC20[] memory, uint256[] memory);
+	function onSushiReward(
+		uint256 pid,
+		address user,
+		address recipient,
+		uint256 sushiAmount,
+		uint256 newLpAmount
+	) external;
+
+	function pendingTokens(
+		uint256 pid,
+		address user,
+		uint256 sushiAmount
+	) external view returns (IERC20[] memory, uint256[] memory);
 }
