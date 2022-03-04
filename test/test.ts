9f39db858a4e94f3a4e680fe123fc799de72d2c8
capybara <capybara@capybara>
2022-03-04 18:21:03 +0530

    chore: stuffs broken

--- test/test.ts
+++ test/test.ts
@@ -614,6 +614,7 @@ describe('Zero', () => {
 	it('MetaRequest test: tests basic metarequest stuff', async () => {
 		const { signer, controller, btcVault } = await getFixtures();
 		enableGlobalMockRuntime();
+		createMockKeeper(signer.provider)
 		const metaRequest = new MetaRequest({
 			module: await ethers.getContract('MetaRequest'),
 			underwriter: await ethers.getContract('DelegateUnderwriter'),
@@ -622,6 +623,8 @@ describe('Zero', () => {
 			contractAddress: controller.address,
 			addressFrom: await signer.getAddress(),
 		});
+		//@ts-ignore
+		console.log(metaRequest.publishMeta.toString())
 		// do stuff with metarequest here
 	});
 });
