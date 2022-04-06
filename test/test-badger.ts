c6223c4d4196b4a88d3387f3cc35877285a81cc5
UndercovercameL <undercover@camel>
2022-04-06 18:13:40 +0530

    chore: script

--- test/test-badger.ts
+++ test/test-badger.ts
@@ -1,6 +1,7 @@
 const hre = require('hardhat');
 const { deployments } = hre;
-const { BigNumber, utils } = hre.ethers;
+//@ts-ignore
+const { BigNumber, utils } = ethers;
 const { UnderwriterTransferRequest, UnderwriterBurnRequest } = require('../');
 const { enableGlobalMockRuntime } = require('../dist/lib/mock');
 var deployParameters = require('../lib/fixtures');
@@ -10,8 +11,11 @@ enableGlobalMockRuntime();
 UnderwriterTransferRequest.prototype.waitForSignature = async function () {
 	await new Promise((resolve) => setTimeout(resolve, 500));
 	return {
+		//@ts-ignore
 		amount: ethers.BigNumber.from(this.amount).sub(ethers.utils.parseUnits('0.0015', 8)).toString(),
+		//@ts-ignore
 		nHash: ethers.utils.hexlify(ethers.utils.randomBytes(32)),
+		//@ts-ignore
 		signature: ethers.utils.hexlify(ethers.utils.randomBytes(65)),
 	};
 };
@@ -38,29 +42,39 @@ describe('BadgerBridgeZeroController', () => {
 		await gateway.mint(utils.randomBytes(32), utils.parseUnits('50', 8), utils.randomBytes(32), '0x'); //mint renBTC to signer
 		const renbtc = new hre.ethers.Contract(
 			deployParameters[process.env.CHAIN].renBTC,
-			[
-				'function approve(address, uint256)'
-			],
+			['function approve(address, uint256)'],
 			signer,
 		);
-		const renCrv = new hre.ethers.Contract('0x93054188d876f558f4a66B2EF1d97d16eDf0895B', ['function exchange(int128, int128, uint256, uint256)'], signer);
+		const renCrv = new hre.ethers.Contract(
+			'0x93054188d876f558f4a66B2EF1d97d16eDf0895B',
+			['function exchange(int128, int128, uint256, uint256)'],
+			signer,
+		);
 		await renbtc.approve(renCrv.address, ethers.constants.MaxUint256);
 		await renCrv.exchange(0, 1, ethers.utils.parseUnits('1', 8), 0);
 		console.log('minted renBTC to signer');
-		await hre.network.provider.send('hardhat_impersonateAccount', [ '0xcf7346a5e41b0821b80d5b3fdc385eeb6dc59f44' ]);
+		await hre.network.provider.send('hardhat_impersonateAccount', ['0xcf7346a5e41b0821b80d5b3fdc385eeb6dc59f44']);
 		const governanceSigner = await hre.ethers.getSigner('0xcf7346a5e41b0821b80d5b3fdc385eeb6dc59f44');
 		await signer.sendTransaction({
 			value: utils.parseEther('0.1'),
-			to: await governanceSigner.getAddress()
+			to: await governanceSigner.getAddress(),
 		});
-		await (new hre.ethers.Contract('0x41671BA1abcbA387b9b2B752c205e22e916BE6e3', [ 'function approveContractAccess(address)' ], governanceSigner)).approveContractAccess((await hre.ethers.getContract('BadgerBridgeZeroController')).address);
-		await hre.network.provider.send('hardhat_impersonateAccount', [ '0xb65cef03b9b89f99517643226d76e286ee999e77' ]);
+		await new hre.ethers.Contract(
+			'0x41671BA1abcbA387b9b2B752c205e22e916BE6e3',
+			['function approveContractAccess(address)'],
+			governanceSigner,
+		).approveContractAccess((await hre.ethers.getContract('BadgerBridgeZeroController')).address);
+		await hre.network.provider.send('hardhat_impersonateAccount', ['0xb65cef03b9b89f99517643226d76e286ee999e77']);
 		const settGovernanceSigner = await hre.ethers.getSigner('0xb65cef03b9b89f99517643226d76e286ee999e77');
 		await signer.sendTransaction({
 			value: utils.parseEther('0.1'),
-			to: await settGovernanceSigner.getAddress()
+			to: await settGovernanceSigner.getAddress(),
 		});
-		await (new hre.ethers.Contract('0x6def55d2e18486b9ddfaa075bc4e4ee0b28c1545', [ 'function approveContractAccess(address)' ], settGovernanceSigner)).approveContractAccess((await hre.ethers.getContract('BadgerBridgeZeroController')).address);
+		await new hre.ethers.Contract(
+			'0x6def55d2e18486b9ddfaa075bc4e4ee0b28c1545',
+			['function approveContractAccess(address)'],
+			settGovernanceSigner,
+		).approveContractAccess((await hre.ethers.getContract('BadgerBridgeZeroController')).address);
 	});
 	it('should do a transfer', async () => {
 		const contractAddress = (await hre.ethers.getContract('BadgerBridgeZeroController')).address;
@@ -82,7 +96,7 @@ describe('BadgerBridgeZeroController', () => {
 		transferRequest.requestType = 'TRANSFER';
 		await transferRequest.sign(signer);
 		console.log('signed', transferRequest.signature);
-		const tx = (await transferRequest.repay(signer));
+		const tx = await transferRequest.repay(signer);
 		console.log((await tx.wait()).gasUsed);
 	});
 	it('should do a burn', async () => {
@@ -98,30 +112,40 @@ describe('BadgerBridgeZeroController', () => {
 			asset: deployParameters[process.env.CHAIN].wBTC,
 			chainId,
 			underwriter: contractAddress,
-			deadline: Math.floor((+new Date() + 1000*60*60*24) / 1000),
-			destination: utils.hexlify(utils.randomBytes(64))
+			deadline: Math.floor((+new Date() + 1000 * 60 * 60 * 24) / 1000),
+			destination: utils.hexlify(utils.randomBytes(64)),
 		});
 		console.log(transferRequest);
 		const { sign, toEIP712 } = transferRequest;
 		transferRequest.sign = async function (signer, contractAddress) {
-		  const asset = this.asset;
-		  this.asset = deployParameters[process.env.CHAIN].renBTC;
-	          const tokenNonce = String(await (new ethers.Contract(this.contractAddress, [ 'function nonces(address) view returns (uint256) '], signer)).nonces(await signer.getAddress()));
-		  this.contractAddress = contractAddress;
-		  transferRequest.toEIP712 = function (...args) {
-	            this.asset = asset;
-	            this.tokenNonce = tokenNonce;
-		    this.assetName = 'WBTC';
-		    return toEIP712.apply(this, args);
-		  };
-		  return await sign.call(this, signer, contractAddress);
+			const asset = this.asset;
+			this.asset = deployParameters[process.env.CHAIN].renBTC;
+			const tokenNonce = String(
+				await new ethers.Contract(
+					this.contractAddress,
+					['function nonces(address) view returns (uint256) '],
+					signer,
+				).nonces(await signer.getAddress()),
+			);
+			this.contractAddress = contractAddress;
+			transferRequest.toEIP712 = function (...args) {
+				this.asset = asset;
+				this.tokenNonce = tokenNonce;
+				this.assetName = 'WBTC';
+				return toEIP712.apply(this, args);
+			};
+			return await sign.call(this, signer, contractAddress);
 		};
 		transferRequest.requestType = 'BURN';
 		await transferRequest.sign(signer, contractAddress);
 		console.log('signed', transferRequest.signature);
-                const wbtc = new ethers.Contract(deployParameters[process.env.CHAIN].wBTC, [ 'function approve(address, uint256)' ], signer);
+		const wbtc = new ethers.Contract(
+			deployParameters[process.env.CHAIN].wBTC,
+			['function approve(address, uint256)'],
+			signer,
+		);
 		await wbtc.approve(transferRequest.contractAddress, ethers.constants.MaxUint256);
-		const tx = (await transferRequest.burn(signer));
+		const tx = await transferRequest.burn(signer);
 		console.log((await tx.wait()).gasUsed);
 	});
 });
