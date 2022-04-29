2a04266aa80b83317f8ee7b7ab30ddf0ac1ce8d9
v <v@v.v>
2022-04-29 17:25:52 +0530

    chore: updated leveldb tests

--- test/leveldb.ts
+++ test/leveldb.ts
@@ -60,6 +60,10 @@ describe('leveldb', () => {
 	it('should store and differentiate between burn/transferrequests', async () => {
 		const [signer] = await ethers.getSigners();
 		const asset = fixtures[process.env.CHAIN].renBTC;
+		BurnRequest.prototype.sign = async function(signer, contractAddress) {
+			this.signature = ethers.utils.hexlify(ethers.utils.randomBytes(64))
+			return this.signature
+		}
 		const { chainId } = await signer.provider.getNetwork();
 		const burnRequest = new BurnRequest({
 			owner: await signer.getAddress(),
@@ -71,14 +75,32 @@ describe('leveldb', () => {
 			chainId,
 			contractAddress: ethers.constants.AddressZero,
 		});
-		await burnRequest.sign(signer, ethers.constants.AddressZero);
+		await burnRequest.sign(signer);
 		process.env.ZERO_PERSISTENCE_DB = '::memory';
 		const persistence = new LevelDBPersistenceAdapter();
 		await persistence.set(burnRequest);
 		burnRequest.amount = ethers.utils.parseUnits('0.04', 8);
 		await burnRequest.sign(signer, ethers.constants.AddressZero);
 		await persistence.set(burnRequest);
-		const requests = await persistence.getAllTransferRequests();
+		let requests = await persistence.getAllTransferRequests();
 		expect(requests.length).to.eql(2);
+
+		const transferRequest = new TransferRequest({
+			to: await signer.getAddress(),
+			amount: ethers.utils.parseUnits('0.05', 8),
+			asset: ethers.constants.AddressZero,
+			nonce: randomNonce(),
+			chainId,
+			pNonce: randomNonce(),
+			data: '0x',
+			module: ethers.constants.AddressZero,
+			underwriter: ethers.constants.AddressZero,
+			contractAddress: ethers.constants.AddressZero,
+		});
+		await transferRequest.sign(signer);
+		await persistence.set(transferRequest)
+		requests = await persistence.getAllTransferRequests();
+		expect(requests.length).to.eql(3)
+		expect(requests.map(d => d.requestType)).to.include.members(['transfer', 'burn'])
 	});
 });
