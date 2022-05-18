6a155bdf97d0b9b01d0697b6cef38654760644be
UndercovercameL <undercover@camel>
2022-05-18 06:43:21 +0000

    chore: changes

---
+++ contracts/libraries/zVaultLib.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+library zVaultLib {
+  struct Isolate {
+    address controller;
+    address governance;
+    address burnModule;
+    mapping(address => bool) whitelist;
+  }
+
+  function toIsolate(uint256 slot)
+    internal
+    pure
+    returns (Isolate storage isolate)
+  {
+    assembly {
+      isolate_slot := key
+    }
+  }
+}
