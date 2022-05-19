223928bd20a33314a63cccf3088df7a0bf0da566
v <v@v.v>
2022-05-19 23:15:01 +0530

    chore: update repay

--- scripts/repay.js
+++ scripts/repay.js
@@ -1,12 +1,31 @@
-const hre = require('hardhat');
-const { UnderwriterTransferRequest } = require('../dist/lib/zero');
+const hre = require("hardhat");
+const { UnderwriterTransferRequest } = require("../dist/lib/zero");
+const path = require("path");
+const { LevelDBPersistenceAdapter } = require("../lib/persistence/leveldb");
 
-const transferRequest = new UnderwriterTransferRequest(require('../g'));
+process.env.ZERO_PERSISTENCE_DB = path
+  .join(
+    process.env.HOME,
+    ".keeper.db"
+  )(async () => {
+    const storage = new LevelDBPersistenceAdapter();
+    const length = await storage.length();
+    const execute = async (i = length) => {
+      const key = await storage.getKeyFromIndex(i);
+      const transferRequest = new UnderwriterTransferRequest(
+        await storage.get(key)
+      );
 
-
-(async () => {
-	const tx = await transferRequest.repay((await hre.ethers.getSigners())[0], { gasLimit: 800000 });
-	console.log(require('util').inspect(tx, { colors: true, depth: 15 }));
-	console.log('waiting ... ');
-	console.log(await tx.wait());
-})().catch((err) => console.error(err));
+      console.log(transferRequest);
+      // const tx = await transferRequest.repay(
+      //   (
+      //     await hre.ethers.getSigners()
+      //   )[0],
+      //   { gasLimit: 800000 }
+      // );
+      // console.log(await tx.wait());
+      // await execute(i - 1)
+    };
+    await execute();
+  })()
+  .catch((err) => console.error(err));
