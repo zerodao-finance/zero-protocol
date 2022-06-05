6e2f7651494ee450477df7f078c4fe45b737bceb
v <v@v.v>
2022-06-05 07:06:23 +0530

    chore: deployed

--- scripts/deploy-badger.js
+++ scripts/deploy-badger.js
@@ -15,26 +15,18 @@ const main = async () => {
   const signer = await getSigner();
   const proxyAbi = await artifacts.readArtifact('TransparentUpgradeableProxy');
 
-  const logic = '0xE182f80DAA9BEE399A623055FC5836BE4781A8eD';
-  const proxyAdmin = '0x1150DB3D2C93bF3E69cFdE29d6F352a35056468c';
-  const governance = '0x3A627ab4F61985f9F353a3F4c2C91dd8c18a9B04';
-  const proxyAdminAbi = await artifacts.readArtifact('ProxyAdmin');
-  const bbAbi = await artifacts.readArtifact('BadgerBridgeZeroControllerArb');
-  const ifaceP = new ethers.utils.Interface(proxyAdminAbi.abi);
-  const newImpl = await deployments.deploy('BadgerBridgeZeroControllerArb', {
-    contract: 'BadgerBridgeZeroControllerArb',
-    from: await signer.getAddress(),
-    args: [],
-    libraries: [],
-    skipIfAlreadyDeployed: false,
-  });
-  console.log(newImpl.receipt.contractAddress);
-  console.log(
-    ifaceP.encodeFunctionData('upgrade', [
-      '0x9880fCd5d42e8F4c2148f2c1187Df050BE3Dbd17',
-      newImpl.receipt.contractAddress,
-    ])
-  );
+  const logic = '0x65906fFe8E9eC966D15B3eCA93313ac496Ac95B4';
+  const proxyAdmin = '0x7CbAe0548ed84017e399976E3997088f31310de3';
+  const governance = '0xC10e4c59F1CC2Bc854A27E645318190F173440cB';
+  const bbAbi = await artifacts.readArtifact('BadgerBridgeZeroControllerAvax');
+  const ifaceP = new ethers.utils.Interface(proxyAbi.abi);
+  const ifaceB = new ethers.utils.Interface(bbAbi.abi);
+  const data = ifaceP.encodeDeploy([
+    logic,
+    proxyAdmin,
+    ifaceB.encodeFunctionData('initialize', [governance, governance]),
+  ]);
+  console.log(data);
   // const newImpl = await deployments.deploy(
   //   'BadgerBridgeZeroControllerDeployer',
   //   {
