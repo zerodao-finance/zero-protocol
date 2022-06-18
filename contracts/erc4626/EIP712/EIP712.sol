9b29abf4bc1cb4c783b1cd25dbaa417b76193d9e
d1ll0n <dillonkellar@protonmail.com>
2022-06-18 17:48:20 -0500

    Add EIP712 contracts

---
+++ contracts/erc4626/EIP712/EIP712.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+import "./AbstractEIP712.sol";
+
+contract EIP712 is AbstractEIP712 {
+  constructor(string memory _name, string memory _version) AbstractEIP712(_name, _version) {}
+
+  function verifyingContract() internal view virtual override returns (address) {
+    return address(this);
+  }
+}
