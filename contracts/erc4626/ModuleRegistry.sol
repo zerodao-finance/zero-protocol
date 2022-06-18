b09485c4d0fa96634887dffb4162fbf61e6f4040
d1ll0n <dillonkellar@protonmail.com>
2022-06-18 18:20:35 -0500

    Only allow module with same asset

--- contracts/erc4626/ModuleRegistry.sol
+++ contracts/erc4626/ModuleRegistry.sol
@@ -3,11 +3,15 @@ pragma solidity >=0.8.0;
 
 import "./storage/ModuleRegistryBase.sol";
 import "./Governable.sol";
+import "./interfaces/IZeroModule.sol";
 
 abstract contract ModuleRegistry is ModuleRegistryBase, Governable {
   function asset() public view virtual returns (address);
 
   function addModule(address module) external onlyGovernance {
+    if (IZeroModule(module).asset() != asset()) {
+      revert ModuleAssetDoesNotMatch(module);
+    }
     approvedModules[module] = true;
     emit ModuleStatusUpdated(module, true);
   }
