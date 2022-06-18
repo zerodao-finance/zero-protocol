6a1a845d9902004eed6cf3a59abdaeafd080e7b7
d1ll0n <dillonkellar@protonmail.com>
2022-06-18 18:12:35 -0500

    Add ReentrancyGuard

---
+++ contracts/erc4626/ReentrancyGuard.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+import "./storage/ReentrancyGuardBase.sol";
+
+/// @notice Gas optimized reentrancy protection for smart contracts.
+/// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/ReentrancyGuard.sol)
+/// @author Modified from OpenZeppelin (https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/ReentrancyGuard.sol)
+abstract contract ReentrancyGuard is ReentrancyGuardBase {
+  constructor() {
+    locked = 1;
+  }
+
+  modifier nonReentrant() virtual {
+    if (locked != 1) {
+      revert Reentrancy();
+    }
+
+    locked = 2;
+    _;
+    locked = 1;
+  }
+}
