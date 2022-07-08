5041d787bdf1b3f38b6067815d265f347f8d49d1
d1ll0n <dillonkellar@protonmail.com>
2022-07-08 11:00:26 -0500

    Improve domain separator compute

--- contracts/erc4626/EIP712/AbstractEIP712.sol
+++ contracts/erc4626/EIP712/AbstractEIP712.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.13;
 
+import "../utils/MemoryRestoration.sol";
+
 uint256 constant EIP712SignaturePrefix = 0x1901000000000000000000000000000000000000000000000000000000000000;
 bytes32 constant _DOMAIN_TYPE_HASH = 0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f;
 
-abstract contract AbstractEIP712 {
+abstract contract AbstractEIP712 is MemoryRestoration {
   uint256 private immutable _CHAIN_ID;
   bytes32 private immutable _DOMAIN_SEPARATOR;
   bytes32 private immutable _NAME_HASH;
@@ -25,8 +27,19 @@ abstract contract AbstractEIP712 {
     }
   }
 
-  function computeDomainSeparator() internal view returns (bytes32) {
-    return keccak256(abi.encode(_DOMAIN_TYPE_HASH, _NAME_HASH, _VERSION_HASH, block.chainid, verifyingContract()));
+  function computeDomainSeparator() internal view returns (bytes32 separator) {
+    address _verifyingContract = verifyingContract();
+    bytes32 nameHash = _NAME_HASH;
+    bytes32 versionHash = _VERSION_HASH;
+    assembly {
+      let ptr := mload(0x40)
+      mstore(ptr, _DOMAIN_TYPE_HASH)
+      mstore(add(ptr, 0x20), nameHash)
+      mstore(add(ptr, 0x40), versionHash)
+      mstore(add(ptr, 0x60), chainid())
+      mstore(add(ptr, 0x80), _verifyingContract)
+      separator := keccak256(ptr, 0xa0)
+    }
   }
 
   function getDomainSeparator() public view virtual returns (bytes32) {
