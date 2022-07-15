d6586b0c0407c4c4c74cda55ae1d65629b44baa6
d1ll0n <dillonkellar@protonmail.com>
2022-07-15 00:01:29 -0500

    Update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,14 @@
 module.exports = {
-  semi: true,
-  trailingComma: 'all',
-  singleQuote: true,
-  printWidth: 120,
-  tabWidth: 2,
-  useTabs: true,
+  overrides: [{
+    files: "*.sol",
+    options: {
+      semi: true,
+      trailingComma: 'all',
+      singleQuote: true,
+      printWidth: 120,
+      tabWidth: 2,
+      useTabs: true,
+      bracketSpacing: true
+    }
+  }]
 };
