08caffdc03a3bea9ced6da270860b3b5a89f20e7
d1ll0n <dillonkellar@protonmail.com>
2022-07-14 23:40:00 -0500

    Rename ModuleFees->ModuleState. Separate btc fee params. Rename refund params.

--- struct-defs/ModuleStateTypeDef.sol
+++ struct-defs/ModuleStateTypeDef.sol
@@ -3,47 +3,60 @@ enum ModuleType {
   LoanOverride,
   LoanAndRepayOverride
 }
-struct ModuleFees {
+struct ModuleState {
   ModuleType moduleType exact;
+
   // gas to refund keeper for executing a loan call (set by governance)
   uint8 loanGasE4;
+
   // gas to refund keeper for executing a repay call (set by governance)
   uint8 repayGasE4;
+
   // ETH paid to keepers for executing a repay call
   // loanGasE4 * 10000 * gasPrice (cached)
-  uint64 loanRefundEth;
+  uint64 ethRefundForLoanGas;
+
   // ETH paid to keepers for executing a repay call
   // repayGasE4 * 10000 * gasPrice (cached)
-  uint64 repayRefundEth;
-  // Static bitcoin fee charged on mint amounts
-  // ((loanRefundEth + repayRefundEth) * btcPrice) + global.loanStaticBitcoinFee (cached)
-  uint24 staticBorrowFee;
+  uint64 ethRefundForRepayGas;
+
+  // BTC value of `ethRefundForLoanGas` (cached)
+  uint24 btcFeeForLoanGas;
+
+  // BTC value of `repayRefundEth` (cached)
+  uint24 btcFeeForRepayGas;
+
   // Last time the cached values were updated
   // Cache must update if it is lower than global update timestamp
   // or is older than the cache ttl
   uint32 lastUpdateTimestamp;
 
   group LoanParams {
+    get;
+
     moduleType exact;
-    loanRefundEth;
-    staticBorrowFee;
+    ethRefundForLoanGas;
+  }
+
+  group BitcoinGasFees {
+    get;
+
+    btcFeeForLoanGas;
+    btcFeeForRepayGas;
   }
 
   group RepayParams {
     moduleType exact;
-    repayRefundEth;
+    ethRefundForRepayGas;
+    btcFeeForRepayGas;
   }
 
   group Cached {
-    loanRefundEth;
-    repayRefundEth;
-    staticBorrowFee;
+    ethRefundForLoanGas;
+    ethRefundForRepayGas;
+    btcFeeForLoanGas;
+    btcFeeForRepayGas;
     lastUpdateTimestamp;
-  }
-
-  group Config {
-    loanGasE4;
-    repayGasE4;
   }
 
   group GasParams {
