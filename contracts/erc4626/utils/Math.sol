30a2ba7deed2b4d70da5dd39692d89c33a214ddc
d1ll0n <dillonkellar@protonmail.com>
2022-07-14 23:41:46 -0500

    add math helper functions

--- contracts/erc4626/utils/Math.sol
+++ contracts/erc4626/utils/Math.sol
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
+import "./CoderConstants.sol";
+
+uint256 constant TenThousand = 1e4;
+uint256 constant OneEth = 1e18;
 
 library Math {
   function avg(uint256 a, uint256 b) internal pure returns (uint256 c) {
@@ -12,6 +16,30 @@ library Math {
 
   function max(uint256 a, uint256 b) internal pure returns (uint256 c) {
     c = ternary(a < b, b, a);
+  }
+
+  function subMinZero(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    unchecked {
+      c = ternary(a > b, a - b, 0);
+    }
+  }
+
+  function uncheckedMulBipsUp(uint256 x, uint256 bips) internal pure returns (uint256 y) {
+    assembly {
+      let numerator := mul(x, bips)
+      y := mul(iszero(iszero(numerator)), add(div(sub(numerator, 1), TenThousand), 1))
+    }
+  }
+
+  // Equivalent to ceil((x)e-4)
+  function uncheckedDivUpE4(uint256 x) internal pure returns (uint256 y) {
+    assembly {
+      y := add(div(sub(x, 1), TenThousand), 1)
+    }
+  }
+
+  function mulBips(uint256 n, uint256 bips) internal pure returns (uint256 result) {
+    result = (n * bips) / TenThousand;
   }
 
   function ternary(
