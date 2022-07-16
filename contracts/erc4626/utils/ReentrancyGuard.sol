13c61228e4892c5256ee990fb6cf21ec9ada8dfd
d1ll0n <dillonkellar@protonmail.com>
2022-07-16 05:24:16 -0500

    Move ReentrancyGuard to utils

--- contracts/erc4626/utils/ReentrancyGuard.sol
+++ contracts/erc4626/utils/ReentrancyGuard.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.13;
-import "./interfaces/ReentrancyErrors.sol";
-import "./storage/ReentrancyGuardStorage.sol";
+import "../interfaces/ReentrancyErrors.sol";
+import "../storage/ReentrancyGuardStorage.sol";
 
 /// @notice Gas optimized reentrancy protection for smart contracts.
 /// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/ReentrancyGuard.sol)
