99d32f87708eae6b260e0fe67cb957563b5216a9
d1ll0n <dillonkellar@protonmail.com>
2022-07-16 11:53:48 -0500

    vaultExpenseWithoutRepayFee -> btcFeeForLoanGas

--- contracts/erc4626/utils/LoanRecordCoder.sol
+++ contracts/erc4626/utils/LoanRecordCoder.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import './CoderConstants.sol';
+import "./CoderConstants.sol";
 
 // ============================== NOTICE ==============================
 // This library was automatically generated with stackpacker.
@@ -13,7 +13,7 @@ import './CoderConstants.sol';
 //   uint48 sharesLocked;
 //   uint48 actualBorrowAmount;
 //   uint48 lenderDebt;
-//   uint48 vaultExpenseWithoutRepayFee;
+//   uint48 btcFeeForLoanGas;
 //   uint32 expiry;
 // }
 type LoanRecord is uint256;
@@ -21,76 +21,73 @@ type LoanRecord is uint256;
 LoanRecord constant DefaultLoanRecord = LoanRecord.wrap(0);
 
 library LoanRecordCoder {
-	/*//////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////
                            LoanRecord
 //////////////////////////////////////////////////////////////*/
 
-	function decode(LoanRecord encoded)
-		internal
-		pure
-		returns (
-			uint256 sharesLocked,
-			uint256 actualBorrowAmount,
-			uint256 lenderDebt,
-			uint256 vaultExpenseWithoutRepayFee,
-			uint256 expiry
-		)
-	{
-		assembly {
-			sharesLocked := shr(LoanRecord_sharesLocked_bitsAfter, encoded)
-			actualBorrowAmount := and(MaxUint48, shr(LoanRecord_actualBorrowAmount_bitsAfter, encoded))
-			lenderDebt := and(MaxUint48, shr(LoanRecord_lenderDebt_bitsAfter, encoded))
-			vaultExpenseWithoutRepayFee := and(MaxUint48, shr(LoanRecord_vaultExpenseWithoutRepayFee_bitsAfter, encoded))
-			expiry := and(MaxUint32, shr(LoanRecord_expiry_bitsAfter, encoded))
-		}
-	}
+  function decode(LoanRecord encoded)
+    internal
+    pure
+    returns (
+      uint256 sharesLocked,
+      uint256 actualBorrowAmount,
+      uint256 lenderDebt,
+      uint256 btcFeeForLoanGas,
+      uint256 expiry
+    )
+  {
+    assembly {
+      sharesLocked := shr(LoanRecord_sharesLocked_bitsAfter, encoded)
+      actualBorrowAmount := and(MaxUint48, shr(LoanRecord_actualBorrowAmount_bitsAfter, encoded))
+      lenderDebt := and(MaxUint48, shr(LoanRecord_lenderDebt_bitsAfter, encoded))
+      btcFeeForLoanGas := and(MaxUint48, shr(LoanRecord_btcFeeForLoanGas_bitsAfter, encoded))
+      expiry := and(MaxUint32, shr(LoanRecord_expiry_bitsAfter, encoded))
+    }
+  }
 
-	function encode(
-		uint256 sharesLocked,
-		uint256 actualBorrowAmount,
-		uint256 lenderDebt,
-		uint256 vaultExpenseWithoutRepayFee,
-		uint256 expiry
-	) internal pure returns (LoanRecord encoded) {
-		assembly {
-			if or(
-				gt(sharesLocked, MaxUint48),
-				or(
-					gt(actualBorrowAmount, MaxUint48),
-					or(gt(lenderDebt, MaxUint48), or(gt(vaultExpenseWithoutRepayFee, MaxUint48), gt(expiry, MaxUint32)))
-				)
-			) {
-				mstore(0, Panic_error_signature)
-				mstore(Panic_error_offset, Panic_arithmetic)
-				revert(0, Panic_error_length)
-			}
-			encoded := or(
-				shl(LoanRecord_sharesLocked_bitsAfter, sharesLocked),
-				or(
-					shl(LoanRecord_actualBorrowAmount_bitsAfter, actualBorrowAmount),
-					or(
-						shl(LoanRecord_lenderDebt_bitsAfter, lenderDebt),
-						or(
-							shl(LoanRecord_vaultExpenseWithoutRepayFee_bitsAfter, vaultExpenseWithoutRepayFee),
-							shl(LoanRecord_expiry_bitsAfter, expiry)
-						)
-					)
-				)
-			)
-		}
-	}
+  function encode(
+    uint256 sharesLocked,
+    uint256 actualBorrowAmount,
+    uint256 lenderDebt,
+    uint256 btcFeeForLoanGas,
+    uint256 expiry
+  ) internal pure returns (LoanRecord encoded) {
+    assembly {
+      if or(
+        gt(sharesLocked, MaxUint48),
+        or(
+          gt(actualBorrowAmount, MaxUint48),
+          or(gt(lenderDebt, MaxUint48), or(gt(btcFeeForLoanGas, MaxUint48), gt(expiry, MaxUint32)))
+        )
+      ) {
+        mstore(0, Panic_error_signature)
+        mstore(Panic_error_offset, Panic_arithmetic)
+        revert(0, Panic_error_length)
+      }
+      encoded := or(
+        shl(LoanRecord_sharesLocked_bitsAfter, sharesLocked),
+        or(
+          shl(LoanRecord_actualBorrowAmount_bitsAfter, actualBorrowAmount),
+          or(
+            shl(LoanRecord_lenderDebt_bitsAfter, lenderDebt),
+            or(shl(LoanRecord_btcFeeForLoanGas_bitsAfter, btcFeeForLoanGas), shl(LoanRecord_expiry_bitsAfter, expiry))
+          )
+        )
+      )
+    }
+  }
 
-	/*//////////////////////////////////////////////////////////////
+  /*//////////////////////////////////////////////////////////////
                   LoanRecord comparison methods
 //////////////////////////////////////////////////////////////*/
 
-	function equals(LoanRecord a, LoanRecord b) internal pure returns (bool _equals) {
-		assembly {
-			_equals := eq(a, b)
-		}
-	}
+  function equals(LoanRecord a, LoanRecord b) internal pure returns (bool _equals) {
+    assembly {
+      _equals := eq(a, b)
+    }
+  }
 
-	function isNull(LoanRecord a) internal pure returns (bool _isNull) {
-		_isNull = equals(a, DefaultLoanRecord);
-	}
+  function isNull(LoanRecord a) internal pure returns (bool _isNull) {
+    _isNull = equals(a, DefaultLoanRecord);
+  }
 }
