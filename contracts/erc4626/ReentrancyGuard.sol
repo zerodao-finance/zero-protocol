02c7339fc281e642d78a222e1d7ccc852798fc24
d1ll0n <dillonkellar@protonmail.com>
2022-07-16 05:21:43 -0500

    Add ReentrancyErrors and initializer. Replace Base with Storage

--- contracts/erc4626/ReentrancyGuard.sol
+++ contracts/erc4626/ReentrancyGuard.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.13;
-
-import "./storage/ReentrancyGuardBase.sol";
+import "./interfaces/ReentrancyErrors.sol";
+import "./storage/ReentrancyGuardStorage.sol";
 
 /// @notice Gas optimized reentrancy protection for smart contracts.
 /// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/ReentrancyGuard.sol)
 /// @author Modified from OpenZeppelin (https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/ReentrancyGuard.sol)
-abstract contract ReentrancyGuard is ReentrancyGuardBase {
-  constructor() {
+abstract contract ReentrancyGuard is ReentrancyGuardStorage, ReentrancyErrors {
+  function _initialize() internal virtual {
     locked = 1;
   }
 
