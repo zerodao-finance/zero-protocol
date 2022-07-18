378ffe9960db53ac610d0c03ba70607a6479757f
d1ll0n <dillonkellar@protonmail.com>
2022-07-18 14:21:06 -0500

    Add getters for btc fee, borrow amount and SharesAndDebt

--- struct-defs/LoanRecordTypeDef.sol
+++ struct-defs/LoanRecordTypeDef.sol
@@ -3,15 +3,22 @@ struct LoanRecord {
   uint48 sharesLocked {}
 
   // Amount of underlying assets transferred out of the vault
-  uint48 actualBorrowAmount {}
+  uint48 actualBorrowAmount { get; }
 
   // Amount of underlying assets the lender is responsible for
   // repaying, equal to borrow amount plus gas and zero fees
   uint48 lenderDebt {}
 
   // Bitcoin value of eth refund paid for the loan() call
-  uint48 btcFeeForLoanGas {}
+  uint48 btcFeeForLoanGas { get; }
 
   // Time at which the loan expires
   uint32 expiry {}
+
+  group SharesAndDebt {
+    get;
+
+    sharesLocked;
+    lenderDebt;
+  }
 }
