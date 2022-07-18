cec064738ad6bfa86f4ea9e317974f129da7ade8
d1ll0n <dillonkellar@protonmail.com>
2022-07-18 13:23:02 -0500

    Add targetEthReserve, maxGasProfitShareBips

--- contracts/erc4626/vault/ZeroBTC.sol
+++ contracts/erc4626/vault/ZeroBTC.sol
@@ -5,6 +5,8 @@ import { ZeroBTCBase } from "./ZeroBTCBase.sol";
 import { ZeroBTCCache } from "./ZeroBTCCache.sol";
 import { ZeroBTCConfig } from "./ZeroBTCConfig.sol";
 import { ZeroBTCLoans } from "./ZeroBTCLoans.sol";
+import { IGateway, IGatewayRegistry } from "../../interfaces/IGatewayRegistry.sol";
+import { IChainlinkOracle } from "../../interfaces/IChainlinkOracle.sol";
 
 contract ZeroBTC is ZeroBTCBase, ZeroBTCCache, ZeroBTCConfig, ZeroBTCLoans {
   constructor(
@@ -13,6 +15,8 @@ contract ZeroBTC is ZeroBTCBase, ZeroBTCCache, ZeroBTCConfig, ZeroBTCLoans {
     IChainlinkOracle gasPriceOracle,
     uint256 cacheTimeToLive,
     uint256 maxLoanDuration,
+    uint256 targetEthReserve,
+    uint256 maxGasProfitShareBips,
     address _asset,
     address _proxyContract
   )
@@ -22,6 +26,8 @@ contract ZeroBTC is ZeroBTCBase, ZeroBTCCache, ZeroBTCConfig, ZeroBTCLoans {
       gasPriceOracle,
       cacheTimeToLive,
       maxLoanDuration,
+      targetEthReserve,
+      maxGasProfitShareBips,
       _asset,
       _proxyContract
     )
