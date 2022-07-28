bf8f71b22ba6d01e0ea1ee3e1ba59e90543c2cb4
v16s <16210686+v16s@users.noreply.github.com>
2022-07-28 18:02:32 +0530

    chore: changes

--- contracts/interfaces/IWETH.sol
+++ contracts/interfaces/IWETH.sol
@@ -2,6 +2,8 @@
 
 pragma solidity >=0.5.0 <0.9.0;
 
-interface IWETH {
+import "@openzeppelin/contracts-new/token/ERC20/IERC20.sol";
+
+interface IWETH is IERC20 {
   function withdraw(uint256) external;
 }
--- contracts/test/BTCVault.test.sol
+++ contracts/test/BTCVault.test.sol
@@ -3,18 +3,20 @@ import "forge-std/Test.sol";
 import { IGateway, IGatewayRegistry } from "../interfaces/IGatewayRegistry.sol";
 import { IChainlinkOracle } from "../interfaces/IChainlinkOracle.sol";
 import { ConvertWBTCMainnet as ConvertWBTC } from "../modules/mainnet/ConvertWBTC.sol";
+import "../util/RenBtcEthConverter.sol";
 import "../erc4626/interfaces/IRenBtcEthConverter.sol";
 import "../erc4626/vault/ZeroBTC.sol";
 
 contract BTCVaultTest is Test {
-  ZeroBTC vault;
+  constructor() {}
 
-  constructor() {
-    vault = new ZeroBTC(
+  function test() public {
+    RenBtcEthConverterMainnet converter = new RenBtcEthConverterMainnet();
+    ZeroBTC vault = new ZeroBTC(
       IGatewayRegistry(0xe4b679400F0f267212D5D812B95f58C83243EE71),
       IChainlinkOracle(0xdeb288F737066589598e9214E782fa5A8eD689e8),
       IChainlinkOracle(0x169E633A2D1E6c10dD91238Ba11c4A708dfEF37C),
-      IRenBtcEthConverter(address(0x0)),
+      IRenBtcEthConverter(address(converter)),
       0,
       0,
       0,
@@ -23,9 +25,6 @@ contract BTCVaultTest is Test {
       address(0x0),
       address(0x0)
     );
-  }
-
-  function test() public {
     ConvertWBTC module = new ConvertWBTC(
       0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D
     );
