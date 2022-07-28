4353008053c60a2037007ffc8d4d4fefb1690541
v16s <16210686+v16s@users.noreply.github.com>
2022-07-28 17:09:06 +0530

    chore: update

--- contracts/test/BTCVault.test.sol
+++ contracts/test/BTCVault.test.sol
@@ -11,9 +11,9 @@ contract BTCVaultTest is Test {
 
   constructor() {
     vault = new ZeroBTC(
-      IGatewayRegistry(address(0x0)),
-      IChainlinkOracle(address(0x0)),
-      IChainlinkOracle(address(0x0)),
+      IGatewayRegistry(0xe4b679400F0f267212D5D812B95f58C83243EE71),
+      IChainlinkOracle(0xdeb288F737066589598e9214E782fa5A8eD689e8),
+      IChainlinkOracle(0x169E633A2D1E6c10dD91238Ba11c4A708dfEF37C),
       IRenBtcEthConverter(address(0x0)),
       0,
       0,
@@ -26,6 +26,8 @@ contract BTCVaultTest is Test {
   }
 
   function test() public {
-    ConvertWBTC module = new ConvertWBTC(address(0x0));
+    ConvertWBTC module = new ConvertWBTC(
+      0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D
+    );
   }
 }
