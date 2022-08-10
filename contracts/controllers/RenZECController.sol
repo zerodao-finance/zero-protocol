3500155d7cba0e4a63868f3ffef2693b9a8b7dd8
v16s <16210686+v16s@users.noreply.github.com>
2022-08-10 11:19:04 +0530

    chore: add controller

---
+++ contracts/controllers/RenZECController.sol
@@ -0,0 +1,315 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0;
+pragma abicoder v2;
+
+import { ISwapRouter } from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import { IQuoter } from "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
+import { ZeroLib } from "../libraries/ZeroLib.sol";
+import { IERC2612Permit } from "../interfaces/IERC2612Permit.sol";
+import { SplitSignatureLib } from "../libraries/SplitSignatureLib.sol";
+import { IWETH } from "../interfaces/IWETH.sol";
+import { IGateway } from "../interfaces/IGateway.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Math } from "@openzeppelin/contracts/math/Math.sol";
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import { ECDSA } from "@openzeppelin/contracts/cryptography/ECDSA.sol";
+import { EIP712Upgradeable } from "@openzeppelin/contracts-upgradeable/drafts/EIP712Upgradeable.sol";
+
+contract RenZECController is EIP712Upgradeable {
+  using SafeERC20 for IERC20;
+  using SafeMath for *;
+  uint256 public fee;
+  address public governance;
+  address public strategist;
+
+  address constant renzec = 0x1C5db575E2Ff833E46a2E9864C22F4B22E0B37C2;
+  address constant zecGateway = 0xc3BbD5aDb611dd74eCa6123F05B18acc886e122D;
+  address constant routerv3 = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+  address constant factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+  address constant weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+  uint24 constant renZECwethFee = 1000;
+  uint256 public governanceFee;
+  bytes32 constant PERMIT_TYPEHASH = 0xea2aa0a1be11a07ed86d755c93467f4f82362b452371d1ba94d1715123511acb;
+  uint256 constant GAS_COST = uint256(42e4);
+  uint256 constant ETH_RESERVE = uint256(5 ether);
+  uint256 internal renzecForOneETHPrice;
+  uint256 internal burnFee;
+  uint256 public keeperReward;
+  uint256 public constant REPAY_GAS_DIFF = 41510;
+  uint256 public constant BURN_GAS_DIFF = 41118;
+
+  function setStrategist(address _strategist) public {
+    require(msg.sender == governance, "!governance");
+    strategist = _strategist;
+  }
+
+  function setGovernance(address _governance) public {
+    require(msg.sender == governance, "!governance");
+    governance = _governance;
+  }
+
+  function computeCalldataGasDiff() internal pure returns (uint256 diff) {
+    if (true) return 0; // TODO: implement exact gas metering
+    // EVM charges less for zero bytes, we must compute the offset for refund
+    // TODO make this efficient
+    uint256 sz;
+    assembly {
+      sz := calldatasize()
+    }
+    diff = sz.mul(uint256(68));
+    bytes memory slice;
+    for (uint256 i = 0; i < sz; i += 0x20) {
+      uint256 word;
+      assembly {
+        word := calldataload(i)
+      }
+      for (uint256 i = 0; i < 256 && ((uint256(~0) << i) & word) != 0; i += 8) {
+        if ((word >> i) & 0xff != 0) diff -= 64;
+      }
+    }
+  }
+
+  function getChainId() internal pure returns (uint256 result) {
+    assembly {
+      result := chainid()
+    }
+  }
+
+  function setParameters(
+    uint256 _governanceFee,
+    uint256 _fee,
+    uint256 _burnFee,
+    uint256 _keeperReward
+  ) public {
+    require(governance == msg.sender, "!governance");
+    governanceFee = _governanceFee;
+    fee = _fee;
+    burnFee = _burnFee;
+    keeperReward = _keeperReward;
+  }
+
+  function initialize(address _governance, address _strategist) public initializer {
+    fee = uint256(25e14);
+    burnFee = uint256(4e15);
+    governanceFee = uint256(5e17);
+    governance = _governance;
+    strategist = _strategist;
+    keeperReward = uint256(1 ether).div(1000);
+    IERC20(weth).safeApprove(routerv3, ~uint256(0) >> 2);
+    IERC20(renzec).safeApprove(routerv3, ~uint256(0) >> 2);
+  }
+
+  function applyRatio(uint256 v, uint256 n) internal pure returns (uint256 result) {
+    result = v.mul(n).div(uint256(1 ether));
+  }
+
+  function quote() internal {
+    bytes memory path = abi.encodePacked(weth, renZECwethFee, renzec);
+    renzecForOneETHPrice = IQuoter.quoteExactInput(path, 1 ether);
+  }
+
+  function renZECtoETH(
+    uint256 minOut,
+    uint256 amountIn,
+    address out
+  ) internal returns (uint256 amountOut) {
+    bytes memory path = abi.encodePacked(renzec, renZECwethFee, weth);
+    ISwapRouter.ExactInputParams memory params = ISwapRouter.ExactInputParams({
+      path: path,
+      recipient: address(this),
+      amountOutMinimum: minOut,
+      amountIn: amountIn,
+      deadline: block.timestamp + 1
+    });
+    amountOut = ISwapRouter(routerv3).exactInput(params);
+    IWETH(weth).withdraw(amountOut);
+    address payable recipient = address(uint160(out));
+    recipient.transfer(amountOut);
+  }
+
+  function fromETHToRenZEC(uint256 minOut, uint256 amountIn) internal returns (uint256) {
+    bytes memory path = abi.encodePacked(weth, renZECwethFee, renzec);
+    ISwapRouter.ExactInputParams memory params = ISwapRouter.ExactInputParams({
+      path: path,
+      recipient: address(this),
+      amountOutMinimum: minOut,
+      amountIn: amountIn,
+      deadline: block.timestamp + 1
+    });
+    return ISwapRouter(routerv3).exactInput(params);
+  }
+
+  function toETH() internal returns (uint256 amountOut) {
+    bytes memory path = abi.encodePacked(renzec, renZECwethFee, weth);
+    ISwapRouter.ExactInputParams memory params = ISwapRouter.ExactInputParams({
+      path: path,
+      recipient: address(this),
+      amountOutMinimum: 1,
+      amountIn: IERC20(renzec).balanceOf(address(this)),
+      deadline: block.timestamp + 1
+    });
+    amountOut = ISwapRouter(routerv3).exactInput(params);
+    IWETH(weth).withdraw(amountOut);
+  }
+
+  receive() external payable {
+    // no-op
+  }
+
+  function earn() public {
+    quote();
+    toETH();
+    uint256 balance = address(this).balance;
+    if (balance > ETH_RESERVE) {
+      uint256 output = balance - ETH_RESERVE;
+      uint256 toGovernance = applyRatio(output, governanceFee);
+      address payable governancePayable = address(uint160(governance));
+      governancePayable.transfer(toGovernance);
+      address payable strategistPayable = address(uint160(strategist));
+      strategistPayable.transfer(output.sub(toGovernance));
+    }
+  }
+
+  function computeRenZECGasFee(uint256 gasCost, uint256 gasPrice) internal view returns (uint256 result) {
+    result = gasCost.mul(tx.gasprice).mul(renzecForOneETHPrice).div(uint256(1 ether));
+  }
+
+  function deductMintFee(uint256 amountIn, uint256 multiplier) internal view returns (uint256 amount) {
+    amount = amountIn.sub(applyFee(amountIn, fee, multiplier));
+  }
+
+  function applyFee(
+    uint256 amountIn,
+    uint256 _fee,
+    uint256 multiplier
+  ) internal view returns (uint256 amount) {
+    amount = computeRenZECGasFee(GAS_COST.add(keeperReward.div(tx.gasprice)), tx.gasprice).add(
+      applyRatio(amountIn, _fee)
+    );
+  }
+
+  struct LoanParams {
+    address to;
+    address asset;
+    uint256 nonce;
+    uint256 amount;
+    address module;
+    address underwriter;
+    bytes data;
+    uint256 minOut;
+    uint256 _mintAmount;
+    uint256 gasDiff;
+  }
+
+  function toTypedDataHash(LoanParams memory params) internal view returns (bytes32 result) {
+    bytes32 digest = _hashTypedDataV4(
+      keccak256(
+        abi.encode(
+          keccak256(
+            "TransferRequest(address asset,uint256 amount,address underwriter,address module,uint256 nonce,bytes data)"
+          ),
+          params.asset,
+          params.amount,
+          params.underwriter,
+          params.module,
+          params.nonce,
+          keccak256(params.data)
+        )
+      )
+    );
+    return digest;
+  }
+
+  function repay(
+    address underwriter,
+    address to,
+    address asset,
+    uint256 amount,
+    uint256 actualAmount,
+    uint256 nonce,
+    address module,
+    bytes32 nHash,
+    bytes memory data,
+    bytes memory signature
+  ) public returns (uint256 amountOut) {
+    require(msg.data.length <= 516, "too much calldata");
+    uint256 _gasBefore = gasleft();
+    LoanParams memory params;
+    {
+      require(module == address(0x0), "!approved-module");
+      params = LoanParams({
+        to: to,
+        asset: asset,
+        amount: amount,
+        nonce: nonce,
+        module: module,
+        underwriter: underwriter,
+        data: data,
+        minOut: 1,
+        _mintAmount: 0,
+        gasDiff: computeCalldataGasDiff()
+      });
+      if (data.length > 0) (params.minOut) = abi.decode(data, (uint256));
+    }
+    bytes32 digest = toTypedDataHash(params);
+
+    params._mintAmount = IGateway(zecGateway).mint(
+      keccak256(abi.encode(params.to, params.nonce, params.module, params.data)),
+      actualAmount,
+      nHash,
+      signature
+    );
+
+    {
+      renZECtoETH(params.minOut, deductMintFee(params._mintAmount, 1), to);
+    }
+    {
+      tx.origin.transfer(
+        Math.min(
+          _gasBefore.sub(gasleft()).add(REPAY_GAS_DIFF).add(params.gasDiff).mul(tx.gasprice).add(keeperReward),
+          address(this).balance
+        )
+      );
+    }
+  }
+
+  function burnETH(uint256 minOut, bytes memory destination) public payable returns (uint256 amountToBurn) {
+    amountToBurn = fromETHToRenZEC(minOut, msg.value.sub(applyRatio(msg.value, burnFee)));
+    IGateway(zecGateway).burn(destination, amountToBurn);
+  }
+
+  function fallbackMint(
+    address underwriter,
+    address to,
+    address asset,
+    uint256 amount,
+    uint256 actualAmount,
+    uint256 nonce,
+    address module,
+    bytes32 nHash,
+    bytes memory data,
+    bytes memory signature
+  ) public {
+    LoanParams memory params = LoanParams({
+      to: to,
+      asset: asset,
+      amount: amount,
+      nonce: nonce,
+      module: module,
+      underwriter: underwriter,
+      data: data,
+      minOut: 1,
+      _mintAmount: 0,
+      gasDiff: 0
+    });
+    bytes32 digest = toTypedDataHash(params);
+    uint256 _actualAmount = IGateway(zecGateway).mint(
+      keccak256(abi.encode(params.to, params.nonce, params.module, params.data)),
+      actualAmount,
+      nHash,
+      signature
+    );
+    IERC20(asset).safeTransfer(to, _actualAmount);
+  }
+}
